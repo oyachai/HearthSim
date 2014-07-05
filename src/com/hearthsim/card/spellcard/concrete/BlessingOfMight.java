6c5c43347ba708e8a8443e73b460e1cc78c3c2ef
oyachai <oyachai@hiroakis-pro.home>
2014-07-05 00:34:07 -0400

    Implemented BlessingOfMight

---
+++ src/com/hearthsim/card/spellcard/concrete/BlessingOfMight.java
@@ -0,0 +1,52 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.util.BoardState;
+
+public class BlessingOfMight extends SpellCard {
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public BlessingOfMight(boolean hasBeenUsed) {
+		super("Assassinate", (byte)1, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public BlessingOfMight() {
+		this(false);
+	}
+
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * Gives a minion +3 attack
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	public BoardState useOn(int thisCardIndex, int playerIndex, int minionIndex, BoardState boardState, Deck deck) {
+		if (minionIndex == 0) {
+			return null;
+		}
+		
+		if (playerIndex == 0) {
+			boardState.getMinion_p0(minionIndex - 1).setAttack((byte)(boardState.getMinion_p0(minionIndex - 1).getAttack() + 3));
+		} else {
+			boardState.getMinion_p1(minionIndex - 1).setAttack((byte)(boardState.getMinion_p1(minionIndex - 1).getAttack() + 3));
+		}
+		return super.useOn(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
---
+++ test/com/hearthsim/test/card/TestBlessingOfMight.java
@@ -0,0 +1,125 @@
+package com.hearthsim.test.card;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.concrete.BlessingOfMight;
+import com.hearthsim.util.BoardState;
+
+public class TestBlessingOfMight {
+
+
+	private BoardState board;
+	private static final byte mana = 2;
+	private static final byte attack0 = 2;
+	private static final byte health0 = 5;
+	private static final byte health1 = 1;
+
+	@Before
+	public void setup() {
+		board = new BoardState();
+		Minion minion0 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion1 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion2 = new Minion("" + 0, mana, attack0, health1, attack0, health1, health1);
+		Minion minion3 = new Minion("" + 0, mana, attack0, (byte)(health0-2), attack0, health0, health0);
+
+		BlessingOfMight fb = new BlessingOfMight();
+		board.placeCard_hand(fb);
+		board.placeMinion_p0(minion0);
+		board.placeMinion_p1(minion1);
+		board.placeMinion_p1(minion2);
+		board.placeMinion_p1(minion3);
+		
+		board.setMana_p0(10);
+	}
+	
+	@Test
+	public void test0() {
+
+		Deck deck = null;
+		
+		Card theCard = board.getCard_hand(0);
+		BoardState res;
+		
+		res = theCard.useOn(0, 1, 0, board, deck);
+		assertTrue(res == null);
+		
+		res = theCard.useOn(0, 0, 0, board, deck);
+		assertTrue(res == null);
+
+		res = theCard.useOn(0, 0, 1, board, deck);
+		assertFalse(res == null);
+		assertTrue(res.getNumCards_hand() == 0);
+		assertTrue(res.getNumMinions_p0() == 1);
+		assertTrue(res.getNumMinions_p1() == 3);
+		assertTrue(res.getMana_p0() == 9);
+		assertTrue(res.getMinion_p0(0).getHealth() == health0);
+		assertTrue(res.getMinion_p0(0).getAttack() == attack0 + 3);
+		assertTrue(res.getMinion_p1(0).getHealth() == health0);
+		assertTrue(res.getMinion_p1(0).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(1).getHealth() == health1);
+		assertTrue(res.getMinion_p1(1).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(2).getHealth() == health0-2);
+		assertTrue(res.getMinion_p1(2).getAttack() == attack0);
+		assertTrue(res.getHero_p0().getHealth() == 30);
+		assertTrue(res.getHero_p1().getHealth() == 30);
+
+	}
+	
+	@Test
+	public void test1() {
+
+		Deck deck = null;
+		
+		Card theCard = board.getCard_hand(0);
+		BoardState res;
+
+		res = theCard.useOn(0, 1, 1, board, deck);
+		assertFalse(res == null);
+		assertTrue(res.getNumCards_hand() == 0);
+		assertTrue(res.getNumMinions_p0() == 1);
+		assertTrue(res.getNumMinions_p1() == 3);
+		assertTrue(res.getMana_p0() == 9);
+		assertTrue(res.getMinion_p0(0).getHealth() == health0);
+		assertTrue(res.getMinion_p0(0).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(0).getHealth() == health0);
+		assertTrue(res.getMinion_p1(0).getAttack() == attack0+3);
+		assertTrue(res.getMinion_p1(1).getHealth() == health1);
+		assertTrue(res.getMinion_p1(1).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(2).getHealth() == health0-2);
+		assertTrue(res.getMinion_p1(2).getAttack() == attack0);
+		assertTrue(res.getHero_p0().getHealth() == 30);
+		assertTrue(res.getHero_p1().getHealth() == 30);
+	}
+	
+	@Test
+	public void test2() {
+
+		Deck deck = null;
+		
+		Card theCard = board.getCard_hand(0);
+		BoardState res;
+
+		res = theCard.useOn(0, 1, 2, board, deck);
+		assertFalse(res == null);
+		assertTrue(res.getNumCards_hand() == 0);
+		assertTrue(res.getNumMinions_p0() == 1);
+		assertTrue(res.getNumMinions_p1() == 3);
+		assertTrue(res.getMana_p0() == 9);
+		assertTrue(res.getMinion_p0(0).getHealth() == health0);
+		assertTrue(res.getMinion_p0(0).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(0).getHealth() == health0);
+		assertTrue(res.getMinion_p1(0).getAttack() == attack0);
+		assertTrue(res.getMinion_p1(1).getHealth() == health1);
+		assertTrue(res.getMinion_p1(1).getAttack() == attack0+3);
+		assertTrue(res.getMinion_p1(2).getHealth() == health0-2);
+		assertTrue(res.getMinion_p1(2).getAttack() == attack0);
+		assertTrue(res.getHero_p0().getHealth() == 30);
+		assertTrue(res.getHero_p1().getHealth() == 30);
+	}
+}
