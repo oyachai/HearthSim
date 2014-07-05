b2e3fca3b4d6025aaff0da3a610bda827e31a4cc
oyachai <oyachai@hiroakis-pro.home>
2014-07-05 13:04:42 -0400

    Implemented Houndmaster
    This is the first minion implementation with Battlecry

---
+++ src/com/hearthsim/card/minion/concrete/Houndmaster.java
@@ -0,0 +1,117 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Beast;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Houndmaster extends Minion {
+
+	public Houndmaster() {
+		this(
+				(byte)4,
+				(byte)4,
+				(byte)3,
+				(byte)4,
+				(byte)3,
+				(byte)3,
+				false,
+				false,
+				false,
+				false,
+				false,
+				false,
+				true,
+				false
+			);
+	}
+	
+	public Houndmaster(	
+							byte mana,
+							byte attack,
+							byte health,
+							byte baseAttack,
+							byte baseHealth,
+							byte maxHealth,
+							boolean taunt,
+							boolean divineShield,
+							boolean windFury,
+							boolean charge,
+							boolean hasAttacked,
+							boolean hasWindFuryAttacked,
+							boolean isInHand,
+							boolean hasBeenUsed) {
+		
+		super(
+			"Houndmaster",
+			mana,
+			attack,
+			health,
+			baseAttack,
+			baseHealth,
+			maxHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	/**
+	 * 
+	 * Override for battlecry
+	 * 
+	 * Battlecry: Give a friendly beast +2/+2 and Taunt
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	public HearthTreeNode<BoardState> useOn(int thisCardIndex, int playerIndex, int minionIndex, HearthTreeNode<BoardState> boardState, Deck deck) {
+		
+		if (hasBeenUsed_) {
+			//Card is already used, nothing to do
+			return null;
+		}
+		
+		if (playerIndex == 1 || minionIndex == 0)
+			return null;
+		
+		if (boardState.data_.getNumMinions_p0() < 7) {
+
+			if (!charge_) {
+				hasAttacked_ = true;
+			}
+			hasBeenUsed_ = true;
+			boardState.data_.setMana_p0(boardState.data_.getMana_p0() - this.mana_);
+			boardState.data_.removeCard_hand(thisCardIndex);
+			boardState.data_.placeMinion_p0(this, minionIndex - 1);
+			
+			for (int index = 0; index < boardState.data_.getNumMinions_p0(); ++index) {
+				if (index != minionIndex - 1) {
+					if (boardState.data_.getMinion_p0(index) instanceof Beast) {
+						HearthTreeNode<BoardState> newState = boardState.addChild(new HearthTreeNode<BoardState>((BoardState)boardState.data_.deepCopy()));
+						newState.data_.getMinion_p0(index).setAttack((byte)(newState.data_.getMinion_p0(index).getAttack() + 2));
+						newState.data_.getMinion_p0(index).setHealth((byte)(newState.data_.getMinion_p0(index).getHealth() + 2));
+						newState.data_.getMinion_p0(index).setTaunt(true);
+					}
+					
+				}
+			}
+			return boardState;
+							
+		} else {
+			return null;				
+		}
+
+	}
+
+}
---
+++ test/com/hearthsim/test/minion/TestHoundmaster.java
@@ -0,0 +1,114 @@
+package com.hearthsim.test.minion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Beast;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.concrete.Houndmaster;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class TestHoundmaster {
+
+
+	private HearthTreeNode<BoardState> board;
+	private static final byte mana = 2;
+	private static final byte attack0 = 2;
+	private static final byte health0 = 5;
+	private static final byte health1 = 1;
+
+	@Before
+	public void setup() {
+		board = new HearthTreeNode<BoardState>(new BoardState());
+		Minion minion0 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion1 = new Beast("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion2 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion3 = new Beast("" + 0, mana, attack0, health1, attack0, health1, health1);
+		Minion minion4 = new Minion("" + 0, mana, attack0, (byte)(health0-2), attack0, health0, health0);
+
+		Houndmaster fb = new Houndmaster();
+		board.data_.placeCard_hand(fb);
+		board.data_.placeMinion_p0(minion0);
+		board.data_.placeMinion_p0(minion1);
+		board.data_.placeMinion_p1(minion2);
+		board.data_.placeMinion_p1(minion3);
+		board.data_.placeMinion_p1(minion4);
+		
+		board.data_.setMana_p0(10);
+	}
+	
+	@Test
+	public void test0() {
+
+
+		Deck deck = null;
+		
+		Card theCard = board.data_.getCard_hand(0);
+		HearthTreeNode<BoardState> res;
+		
+		res = theCard.useOn(0, 1, 0, board, deck);
+		assertTrue(res == null);
+		
+		res = theCard.useOn(0, 0, 0, board, deck);
+		assertTrue(res == null);
+
+		res = theCard.useOn(0, 0, 1, board, deck);
+		assertFalse(res == null);
+		assertTrue(res.data_.getNumCards_hand() == 0);
+		assertTrue(res.data_.getNumMinions_p0() == 3);
+		assertTrue(res.data_.getNumMinions_p1() == 3);
+		assertTrue(res.data_.getMana_p0() == 6);
+		
+		assertTrue(res.data_.getMinion_p0(0).getHealth() == 3);
+		assertTrue(res.data_.getMinion_p0(0).getAttack() == 4);
+		assertFalse(res.data_.getMinion_p0(0).getTaunt());
+		assertTrue(res.data_.getMinion_p0(1).getHealth() == health0);
+		assertTrue(res.data_.getMinion_p0(1).getAttack() == attack0);
+		assertFalse(res.data_.getMinion_p0(1).getTaunt());
+		assertTrue(res.data_.getMinion_p0(2).getHealth() == health0);
+		assertTrue(res.data_.getMinion_p0(2).getAttack() == attack0);
+		assertFalse(res.data_.getMinion_p0(2).getTaunt());
+
+		assertTrue(res.data_.getMinion_p1(0).getHealth() == health0);
+		assertTrue(res.data_.getMinion_p1(0).getAttack() == attack0);
+		assertFalse(res.data_.getMinion_p1(0).getTaunt());
+		assertTrue(res.data_.getMinion_p1(1).getHealth() == health1);
+		assertTrue(res.data_.getMinion_p1(1).getAttack() == attack0);
+		assertFalse(res.data_.getMinion_p1(1).getTaunt());
+		assertTrue(res.data_.getMinion_p1(2).getHealth() == health0-2);
+		assertTrue(res.data_.getMinion_p1(2).getAttack() == attack0);
+		assertFalse(res.data_.getMinion_p1(2).getTaunt());
+		
+		assertTrue(res.data_.getHero_p0().getHealth() == 30);
+		assertTrue(res.data_.getHero_p1().getHealth() == 30);
+
+		assertTrue(res.numChildren() == 1);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(0).getHealth() == 3);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(0).getAttack() == 4);
+		assertFalse(res.getChildren().get(0).data_.getMinion_p0(0).getTaunt());
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(1).getHealth() == health0);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(1).getAttack() == attack0);
+		assertFalse(res.getChildren().get(0).data_.getMinion_p0(1).getTaunt());
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(2).getHealth() == health0 + 2);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(2).getAttack() == attack0 + 2);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p0(2).getTaunt());
+		
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(0).getHealth() == health0);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(0).getAttack() == attack0);
+		assertFalse(res.getChildren().get(0).data_.getMinion_p1(0).getTaunt());
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(1).getHealth() == health1);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(1).getAttack() == attack0);
+		assertFalse(res.getChildren().get(0).data_.getMinion_p1(1).getTaunt());
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(2).getHealth() == health0-2);
+		assertTrue(res.getChildren().get(0).data_.getMinion_p1(2).getAttack() == attack0);
+		assertFalse(res.getChildren().get(0).data_.getMinion_p1(2).getTaunt());
+		
+	}
+
+
+}
