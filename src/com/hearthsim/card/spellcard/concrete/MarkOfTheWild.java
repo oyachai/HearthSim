e6813d5a765010182bafd60afd2c5f5820265ac1
oyachai <oyachai@gmail.com>
2014-07-08 14:10:16 -0400

    Implemented Mark of the Wild

---
+++ src/com/hearthsim/card/spellcard/concrete/MarkOfTheWild.java
@@ -0,0 +1,65 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class MarkOfTheWild extends SpellCard {
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public MarkOfTheWild(boolean hasBeenUsed) {
+		super("Mark of the Wild", (byte)2, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public MarkOfTheWild() {
+		this(false);
+	}
+
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * This card heals the target minion up to full health and gives it taunt.  Cannot be used on heroes.
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (minionIndex == 0) {
+			//cant't use it on the heroes
+			return null;
+		}
+		
+		Minion targetMinion = boardState.data_.getMinion(playerIndex, minionIndex - 1);
+		targetMinion.setAttack((byte)(targetMinion.getAttack() + 2));
+		targetMinion.setHealth((byte)(targetMinion.getHealth() + 2));
+		targetMinion.setMaxHealth((byte)(targetMinion.getMaxHealth() + 2));
+		targetMinion.setTaunt(true);
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
---
+++ test/com/hearthsim/test/card/TestMarkOfTheWild.java
@@ -0,0 +1,131 @@
+package com.hearthsim.test.card;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.concrete.MarkOfTheWild;
+import com.hearthsim.card.spellcard.concrete.TheCoin;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class TestMarkOfTheWild {
+
+	
+	
+
+	private HearthTreeNode<BoardState> board;
+	private Deck deck;
+	private static final byte mana = 2;
+	private static final byte attack0 = 5;
+	private static final byte health0 = 3;
+	private static final byte health1 = 7;
+
+	@Before
+	public void setup() {
+		board = new HearthTreeNode<BoardState>(new BoardState());
+
+		Minion minion0_0 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion0_1 = new Minion("" + 0, mana, attack0, (byte)(health1 - 1), attack0, health1, health1);
+		Minion minion1_0 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
+		Minion minion1_1 = new Minion("" + 0, mana, attack0, (byte)(health1 - 1), attack0, health1, health1);
+		
+		board.data_.placeMinion_p0(minion0_0);
+		board.data_.placeMinion_p0(minion0_1);
+		
+		board.data_.placeMinion_p1(minion1_0);
+		board.data_.placeMinion_p1(minion1_1);
+		
+		Card cards[] = new Card[10];
+		for (int index = 0; index < 10; ++index) {
+			cards[index] = new TheCoin();
+		}
+	
+		deck = new Deck(cards);
+
+		MarkOfTheWild fb = new MarkOfTheWild();
+		board.data_.placeCard_hand_p0(fb);
+
+		board.data_.setMana_p0((byte)4);
+		board.data_.setMana_p1((byte)4);
+		
+		board.data_.setMaxMana_p0((byte)4);
+		board.data_.setMaxMana_p1((byte)4);
+		
+	}
+	
+	@Test
+	public void test0() throws HSInvalidPlayerIndexException {
+		
+		Card theCard = board.data_.getCard_hand_p0(0);
+		HearthTreeNode<BoardState> ret = theCard.useOn(0, 1, 0, board, deck);
+		
+		assertTrue(ret == null);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 30);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), health0);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), health1 - 1);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), health0);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), health1 - 1);
+	}
+	
+	@Test
+	public void test1() throws HSInvalidPlayerIndexException {
+		
+		Card theCard = board.data_.getCard_hand_p0(0);
+		HearthTreeNode<BoardState> ret = theCard.useOn(0, 0, 0, board, deck);
+		
+		assertTrue(ret == null);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 30);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), health0);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), health1 - 1);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), health0);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), health1 - 1);
+	}
+	
+	@Test
+	public void test2() throws HSInvalidPlayerIndexException {
+		
+		Card theCard = board.data_.getCard_hand_p0(0);
+		HearthTreeNode<BoardState> ret = theCard.useOn(0, 0, 1, board, deck);
+		
+		assertFalse(ret == null);
+		assertEquals(board.data_.getNumCards_hand(), 0);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 30);
+		assertEquals(board.data_.getMana_p0(), 2);
+		assertEquals(board.data_.getMana_p1(), 4);
+		assertEquals(board.data_.getMaxMana_p0(), 4);
+		assertEquals(board.data_.getMaxMana_p1(), 4);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), health0 + 2);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), health1 - 1);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), health0);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), health1 - 1);
+		assertEquals(board.data_.getMinion_p0(0).getAttack(), attack0 + 2);
+		assertEquals(board.data_.getMinion_p0(1).getAttack(), attack0);
+		assertEquals(board.data_.getMinion_p1(0).getAttack(), attack0);
+		assertEquals(board.data_.getMinion_p1(1).getAttack(), attack0);
+		assertTrue(board.data_.getMinion_p0(0).getTaunt());
+		assertFalse(board.data_.getMinion_p0(1).getTaunt());
+		assertFalse(board.data_.getMinion_p1(0).getTaunt());
+		assertFalse(board.data_.getMinion_p1(1).getTaunt());
+	}
+	
+
+}
