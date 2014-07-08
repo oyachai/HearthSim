74390a7b694d486a9ba4735d5f89d28cceb71ff1
oyachai <oyachai@gmail.com>
2014-07-08 13:45:35 -0400

    Implemented Innervate

---
+++ src/com/hearthsim/card/spellcard/concrete/Innervate.java
@@ -0,0 +1,61 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Innervate extends SpellCard {
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public Innervate(boolean hasBeenUsed) {
+		super("Innervate", (byte)0, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public Innervate() {
+		this(false);
+	}
+	
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * Gives the hero 2 extra mana this turn.  If already at 10 mana, does nothing but the card still disappears.
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (playerIndex > 0 || minionIndex > 0) 
+			return null;
+
+		if (boardState.data_.getMana_p0() < 8)
+			boardState.data_.setMana_p0(boardState.data_.getMana_p0() + 2);
+		else
+			boardState.data_.setMana_p0(10);
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+	
+}
