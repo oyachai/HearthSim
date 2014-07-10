bd38d336283c71c22600e6ae1280139adfa94d10
oyachai <oyachai@gmail.com>
2014-07-10 10:55:35 -0400

    implemented Hunter's Mark

---
+++ src/com/hearthsim/card/spellcard/concrete/HuntersMark.java
@@ -0,0 +1,62 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class HuntersMark extends SpellCard {
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public HuntersMark(boolean hasBeenUsed) {
+		super("Hunter's Mark", (byte)0, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public HuntersMark() {
+		this(false);
+	}
+
+	@Override
+	public Object deepCopy() {
+		return new HuntersMark(this.hasBeenUsed_);
+	}
+
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * This card damages all enemy minions by 1
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (minionIndex == 0) {
+			return null;
+		}
+		boardState.data_.getMinion(playerIndex, minionIndex - 1).setHealth((byte)1);
+		boardState.data_.getMinion(playerIndex, minionIndex - 1).setMaxHealth((byte)1);
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
