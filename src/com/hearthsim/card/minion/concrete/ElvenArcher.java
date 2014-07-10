210917dd871f8fb6e603812911530b537947da13
oyachai <oyachai@gmail.com>
2014-07-09 21:52:48 -0400

    Some code cleanup

--- src/com/hearthsim/card/minion/concrete/ElvenArcher.java
+++ src/com/hearthsim/card/minion/concrete/ElvenArcher.java
@@ -1,7 +1,6 @@
 package com.hearthsim.card.minion.concrete;
 
 import com.hearthsim.card.Deck;
-import com.hearthsim.card.minion.Beast;
 import com.hearthsim.card.minion.Minion;
 import com.hearthsim.exception.HSInvalidPlayerIndexException;
 import com.hearthsim.util.BoardState;
