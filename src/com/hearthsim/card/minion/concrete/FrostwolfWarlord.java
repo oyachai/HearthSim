732515a563407d4b1f2051774a895d06cf9c42db
oyachai <oyachai@gmail.com>
2014-07-09 22:30:16 -0400

    Implemented Frostwolf Warlord

---
+++ src/com/hearthsim/card/minion/concrete/FrostwolfWarlord.java
@@ -0,0 +1,164 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class FrostwolfWarlord extends Minion {
+
+	private static final String NAME = "Frostwolf Warlord";
+	private static final byte MANA_COST = 5;
+	private static final byte ATTACK = 4;
+	private static final byte HEALTH = 4;
+	
+	private static final boolean TAUNT = false;
+	private static final boolean DIVINE_SHIELD = false;
+	private static final boolean WINDFURY = false;
+	private static final boolean CHARGE = false;
+	
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	
+	public FrostwolfWarlord() {
+		this(
+				MANA_COST,
+				ATTACK,
+				HEALTH,
+				ATTACK,
+				(byte)0,
+				HEALTH,
+				HEALTH,
+				TAUNT,
+				DIVINE_SHIELD,
+				WINDFURY,
+				CHARGE,
+				false,
+				false,
+				false,
+				SUMMONED,
+				TRANSFORMED,
+				false,
+				false,
+				true,
+				false
+			);
+	}
+	
+	public FrostwolfWarlord(	
+			byte mana,
+			byte attack,
+			byte health,
+			byte baseAttack,
+			byte extraAttackUntilTurnEnd,
+			byte baseHealth,
+			byte maxHealth,
+			boolean taunt,
+			boolean divineShield,
+			boolean windFury,
+			boolean charge,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean summoned,
+			boolean transformed,
+			boolean destroyOnTurnStart,
+			boolean destroyOnTurnEnd,
+			boolean isInHand,
+			boolean hasBeenUsed) {
+		
+		super(
+			NAME,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			baseHealth,
+			maxHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new FrostwolfWarlord(
+				this.mana_,
+				this.attack_,
+				this.health_,
+				this.baseAttack_,
+				this.extraAttackUntilTurnEnd_,
+				this.baseHealth_,
+				this.maxHealth_,
+				this.taunt_,
+				this.divineShield_,
+				this.windFury_,
+				this.charge_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.summoned_,
+				this.transformed_,
+				this.destroyOnTurnStart_,
+				this.destroyOnTurnEnd_,
+				this.isInHand_,
+				this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Override for battlecry
+	 * 
+	 * Battlecry: gain +1/+1 for each friendly minion on the battlefield
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	public HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		
+		if (hasBeenUsed_) {
+			//Card is already used, nothing to do
+			return null;
+		}
+		
+		if (playerIndex == 1 || minionIndex == 0)
+			return null;
+		
+		if (boardState.data_.getNumMinions_p0() >= 7)
+			return null;
+		
+		int numBuffs = boardState.data_.getNumMinions_p0();
+		HearthTreeNode<BoardState> toRet = super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+
+		Minion placedMinion = toRet.data_.getMinion(playerIndex, minionIndex - 1);
+		placedMinion.setAttack((byte)(placedMinion.getAttack() + numBuffs));
+		placedMinion.setHealth((byte)(placedMinion.getHealth() + numBuffs));
+		placedMinion.setMaxHealth((byte)(placedMinion.getMaxHealth() + numBuffs));
+		return toRet;
+	}
+}
