77ed01b12d1c8db1503a964b66891d58b4faf3e8
oyachai <oyachai@gmail.com>
2014-07-10 11:35:19 -0400

    Implemented Kobold Geomancer
    same as Dalaran Mage

---
+++ src/com/hearthsim/card/minion/concrete/KoboldGeomancer.java
@@ -0,0 +1,122 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.MinionWithSpellDamage;
+
+public class KoboldGeomancer extends MinionWithSpellDamage {
+
+
+	private static final String NAME = "Kobold Geomancer";
+	private static final byte MANA_COST = 2;
+	private static final byte ATTACK = 2;
+	private static final byte HEALTH = 2;
+	
+	private static final boolean TAUNT = false;
+	private static final boolean DIVINE_SHIELD = false;
+	private static final boolean WINDFURY = false;
+	private static final boolean CHARGE = false;
+	
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	
+	private static final byte SPELL_DAMAGE = 1;
+	
+	public KoboldGeomancer() {
+		this(
+				MANA_COST,
+				ATTACK,
+				HEALTH,
+				ATTACK,
+				(byte)0,
+				HEALTH,
+				HEALTH,
+				SPELL_DAMAGE,
+				TAUNT,
+				DIVINE_SHIELD,
+				WINDFURY,
+				CHARGE,
+				false,
+				false,
+				false,
+				SUMMONED,
+				TRANSFORMED,
+				false,
+				false,
+				true,
+				false
+			);
+	}
+	
+	public KoboldGeomancer(	
+			byte mana,
+			byte attack,
+			byte health,
+			byte baseAttack,
+			byte extraAttackUntilTurnEnd,
+			byte baseHealth,
+			byte maxHealth,
+			byte spellDamage,
+			boolean taunt,
+			boolean divineShield,
+			boolean windFury,
+			boolean charge,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean summoned,
+			boolean transformed,
+			boolean destroyOnTurnStart,
+			boolean destroyOnTurnEnd,
+			boolean isInHand,
+			boolean hasBeenUsed) {
+		
+		super(
+			NAME,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			baseHealth,
+			maxHealth,
+			spellDamage,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new KoboldGeomancer(
+				this.mana_,
+				this.attack_,
+				this.health_,
+				this.baseAttack_,
+				this.extraAttackUntilTurnEnd_,
+				this.baseHealth_,
+				this.maxHealth_,
+				this.spellDamage_,
+				this.taunt_,
+				this.divineShield_,
+				this.windFury_,
+				this.charge_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.summoned_,
+				this.transformed_,
+				this.destroyOnTurnStart_,
+				this.destroyOnTurnEnd_,
+				this.isInHand_,
+				this.hasBeenUsed_);
+	}
+}
