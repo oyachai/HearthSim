6ca43261e9456b096e74e21284764aadcfcc5dd2
oyachai <oyachai@hiroakis-pro.home>
2014-07-11 01:16:29 -0400

    bug fix

--- src/com/hearthsim/card/minion/concrete/Succubus.java
+++ src/com/hearthsim/card/minion/concrete/Succubus.java
@@ -2,6 +2,7 @@ package com.hearthsim.card.minion.concrete;
 
 import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
 import com.hearthsim.util.BoardState;
 import com.hearthsim.util.HearthTreeNode;
 
