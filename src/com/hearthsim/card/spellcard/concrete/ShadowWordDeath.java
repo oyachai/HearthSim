fcbed6824ac992cd98dae8a579284215e8ac7d9c
oyachai <oyachai@gmail.com>
2014-07-10 22:26:27 -0400

    Implemented Shadow Word: Death

---
+++ src/com/hearthsim/card/spellcard/concrete/ShadowWordDeath.java
@@ -0,0 +1,70 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class ShadowWordDeath extends SpellCard {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public ShadowWordDeath(boolean hasBeenUsed) {
+		super("Shadow Word: Death", (byte)3, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public ShadowWordDeath() {
+		this(false);
+	}
+
+	@Override
+	public Object deepCopy() {
+		return new ShadowWordDeath(this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * Gives all friendly characters +2 attack for this turn
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (minionIndex == 0) {
+			return null;
+		}
+		
+		Minion targetMinion = boardState.data_.getMinion(playerIndex, minionIndex - 1);
+		if (targetMinion.getAttack() + targetMinion.getExtraAttackUntilTurnEnd() < 5)
+			return null;
+		
+		targetMinion.destroyed(playerIndex, minionIndex, boardState, deck);
+		boardState.data_.removeMinion(playerIndex, minionIndex - 1);
+		
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
