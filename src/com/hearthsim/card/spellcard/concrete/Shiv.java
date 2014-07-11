a8cb034a31f9de318c22370940b839429008e914
oyachai <oyachai@hiroakis-pro.home>
2014-07-11 00:37:24 -0400

    implemented Shiv

---
+++ src/com/hearthsim/card/spellcard/concrete/Shiv.java
@@ -0,0 +1,65 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.card.spellcard.SpellDamage;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Shiv extends SpellDamage {
+
+
+	public Shiv() {
+		this(false);
+	}
+
+	public Shiv(boolean hasBeenUsed) {
+		super("Shiv", (byte)2, (byte)1, hasBeenUsed);
+	}
+
+	@Override
+	public Object deepCopy() {
+		return new Shiv(this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * Deals 2 damage and heals the hero for 2.
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (playerIndex == 0 && minionIndex == 0) 
+			return null;
+		
+		HearthTreeNode<BoardState> toRet = super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+		Card card = deck.drawCard(boardState.data_.getDeckPos_p0());
+		if (card == null) {
+			byte fatigueDamage = boardState.data_.getFatigueDamage_p0();
+			boardState.data_.setFatigueDamage_p0((byte)(fatigueDamage + 1));
+			boardState.data_.getHero_p0().setHealth((byte)(boardState.data_.getHero_p0().getHealth() - fatigueDamage));
+		} else {
+			boardState.data_.placeCard_hand_p0(card);
+			boardState.data_.setDeckPos_p0(boardState.data_.getDeckPos_p0() + 1);
+		}
+		
+		return toRet;
+	}
+}
