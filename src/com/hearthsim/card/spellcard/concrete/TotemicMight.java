cc8ff85fd85fd911439f5f6c19242ebaae5f9f9b
oyachai <oyachai@gmail.com>
2014-07-11 11:28:04 -0400

    Implemented Totemic Might
    … even though I have no totems implemented.  Oh well.

---
+++ src/com/hearthsim/card/spellcard/concrete/TotemicMight.java
@@ -0,0 +1,70 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.Totem;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class TotemicMight extends SpellCard {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public TotemicMight(boolean hasBeenUsed) {
+		super("Totemic Might", (byte)0, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public TotemicMight() {
+		this(false);
+	}
+
+	@Override
+	public Object deepCopy() {
+		return new TotemicMight(this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * Gives all friendly characters +2 attack for this turn
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (minionIndex > 0 || playerIndex == 1) {
+			return null;
+		}
+		
+		for (Minion minion : boardState.data_.getMinions_p0()) {
+			if (minion instanceof Totem) {
+				minion.setHealth((byte)(2 + minion.getHealth()));
+				minion.setMaxHealth((byte)(2 + minion.getMaxHealth()));
+			}
+		}
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
