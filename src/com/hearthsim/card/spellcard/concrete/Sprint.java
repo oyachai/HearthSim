09f6591e6d691522b35f20bd991d02b8dca20b99
oyachai <oyachai@hiroakis-pro.home>
2014-07-11 00:50:00 -0400

    implemented Sprint

---
+++ src/com/hearthsim/card/spellcard/concrete/Sprint.java
@@ -0,0 +1,76 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Sprint extends SpellCard {
+
+
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public Sprint(boolean hasBeenUsed) {
+		super("Sprint", (byte)7, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public Sprint() {
+		this(false);
+	}
+
+	@Override
+	public Object deepCopy() {
+		return new Sprint(this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Use the card on the given target
+	 * 
+	 * This card draws 2 cards from the deck.
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (playerIndex == 1 || minionIndex > 0) {
+			return null;
+		}
+		
+		for (int index = 0; index < 4; ++index) {
+			Card card = deck.drawCard(boardState.data_.getDeckPos_p0());
+			if (card == null) {
+				byte fatigueDamage = boardState.data_.getFatigueDamage_p0();
+				boardState.data_.setFatigueDamage_p0((byte)(fatigueDamage + 1));
+				boardState.data_.getHero_p0().setHealth((byte)(boardState.data_.getHero_p0().getHealth() - fatigueDamage));
+			} else {
+				boardState.data_.placeCard_hand_p0(card);
+				boardState.data_.setDeckPos_p0(boardState.data_.getDeckPos_p0() + 1);
+			}
+		}
+
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
