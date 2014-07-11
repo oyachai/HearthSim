5e5c68e8878239f0c8abc8b588bc82d7356dc7da
oyachai <oyachai@gmail.com>
2014-07-11 14:53:13 -0400

    implemented Water Elemental

---
+++ src/com/hearthsim/card/minion/concrete/WaterElemental.java
@@ -0,0 +1,150 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class WaterElemental extends Minion {
+
+
+	private static final String NAME = "Water Elemental";
+	private static final byte MANA_COST = 4;
+	private static final byte ATTACK = 3;
+	private static final byte HEALTH = 6;
+	
+	private static final boolean TAUNT = false;
+	private static final boolean DIVINE_SHIELD = false;
+	private static final boolean WINDFURY = false;
+	private static final boolean CHARGE = false;
+	
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	
+	public WaterElemental() {
+		this(
+				MANA_COST,
+				ATTACK,
+				HEALTH,
+				ATTACK,
+				(byte)0,
+				HEALTH,
+				HEALTH,
+				TAUNT,
+				DIVINE_SHIELD,
+				WINDFURY,
+				CHARGE,
+				false,
+				false,
+				false,
+				SUMMONED,
+				TRANSFORMED,
+				false,
+				false,
+				true,
+				false
+			);
+	}
+	
+	public WaterElemental(	
+			byte mana,
+			byte attack,
+			byte health,
+			byte baseAttack,
+			byte extraAttackUntilTurnEnd,
+			byte baseHealth,
+			byte maxHealth,
+			boolean taunt,
+			boolean divineShield,
+			boolean windFury,
+			boolean charge,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean summoned,
+			boolean transformed,
+			boolean destroyOnTurnStart,
+			boolean destroyOnTurnEnd,
+			boolean isInHand,
+			boolean hasBeenUsed) {
+		
+		super(
+			NAME,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			baseHealth,
+			maxHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new WaterElemental(
+				this.mana_,
+				this.attack_,
+				this.health_,
+				this.baseAttack_,
+				this.extraAttackUntilTurnEnd_,
+				this.baseHealth_,
+				this.maxHealth_,
+				this.taunt_,
+				this.divineShield_,
+				this.windFury_,
+				this.charge_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.summoned_,
+				this.transformed_,
+				this.destroyOnTurnStart_,
+				this.destroyOnTurnEnd_,
+				this.isInHand_,
+				this.hasBeenUsed_);
+	}
+	
+	/**
+	 * 
+	 * Attack with the minion
+	 * 
+	 * Any minion attacked by this minion is frozen.
+	 * 
+	 * @param thisMinionIndex Attacking minion's index (note: attacking player index is assumed to be 0)
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> attack_core(
+			int thisMinionIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (minionIndex == 0)
+			boardState.data_.getHero_p1().setFrozen(true);
+		else
+			boardState.data_.getMinion(playerIndex, minionIndex - 1).setFrozen(true);
+		HearthTreeNode<BoardState> toRet = super.attack_core(thisMinionIndex, playerIndex, minionIndex, boardState, deck);
+		return toRet;
+	}
+}
