d0376001a8cd21d01f30a4f59044bfa17116429d
oyachai <oyachai@gmail.com>
2014-07-14 14:13:06 -0400

    implemented Druid Hero class

---
+++ src/com/hearthsim/card/minion/heroes/Druid.java
@@ -0,0 +1,86 @@
+package com.hearthsim.card.minion.heroes;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Hero;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.DeepCopyable;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Druid extends Hero {
+
+	public Druid() {
+		this("", (byte)30);
+	}
+
+	public Druid(String name, byte health) {
+		this(name, (byte)0, (byte)0, health, (byte)0, (byte)0, false, false, false, false, false);
+	}
+	
+	public Druid(
+			String name,
+			byte attack,
+			byte extraAttackUntilTurnEnd,
+			byte health,
+			byte armor,
+			byte weaponCharge,
+			boolean windFury,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean hasBeenUsed) {
+	
+		super(name, attack, extraAttackUntilTurnEnd, health, armor, weaponCharge, windFury, hasAttacked, hasWindFuryAttacked, frozen, hasBeenUsed);
+	}
+	
+	@Override
+	public DeepCopyable deepCopy() {
+		return new Druid(
+				this.name_, 
+				this.attack_,
+				this.extraAttackUntilTurnEnd_,
+				this.health_,
+				this.armor_,
+				this.weaponCharge_,
+				this.windFury_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.hasBeenUsed_
+				);
+	}
+	
+	/**
+	 * Use the hero ability on a given target
+	 * 
+	 * Priest: Heals a target for 2
+	 * 
+	 * @param thisPlayerIndex The player index of the hero
+	 * @param targetPlayerIndex The player index of the target character
+	 * @param targetMinionIndex The minion index of the target character
+	 * @param boardState
+	 * @param deck
+	 * @return
+	 */
+	@Override
+	public HearthTreeNode<BoardState> useHeroAbility(
+			int thisPlayerIndex,
+			int targetPlayerIndex,
+			int targetMinionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSException
+	{
+		HearthTreeNode<BoardState> toRet = null;
+		if (targetMinionIndex == 0 && targetPlayerIndex == 0) {
+			toRet = super.useHeroAbility(thisPlayerIndex, targetPlayerIndex, targetMinionIndex, boardState, deck);
+			if (toRet != null) {
+				Hero target = boardState.data_.getHero_p0();
+				target.setExtraAttackUntilTurnEnd((byte)1);
+				target.setArmor((byte)1);
+			}
+		}
+		return toRet;
+	}
+
+}
