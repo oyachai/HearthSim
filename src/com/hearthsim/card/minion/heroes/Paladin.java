ec6b43d4659f956a5ca00a6115e039cc0c6fb6dd
oyachai <oyachai@hiroakis-pro.home>
2014-07-14 21:02:16 -0400

    implemented Paladin Hero class

---
+++ src/com/hearthsim/card/minion/heroes/Paladin.java
@@ -0,0 +1,94 @@
+package com.hearthsim.card.minion.heroes;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Hero;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.concrete.SilverHandRecruit;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.DeepCopyable;
+import com.hearthsim.util.HearthTreeNode;
+
+public class Paladin extends Hero {
+
+	public Paladin() {
+		this("", (byte)30);
+	}
+
+	public Paladin(String name, byte health) {
+		this(name, (byte)0, (byte)0, health, (byte)0, (byte)0, false, false, false, false, false);
+	}
+	
+	public Paladin(
+			String name,
+			byte attack,
+			byte extraAttackUntilTurnEnd,
+			byte health,
+			byte armor,
+			byte weaponCharge,
+			boolean windFury,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean hasBeenUsed) {
+	
+		super(name, attack, extraAttackUntilTurnEnd, health, armor, weaponCharge, windFury, hasAttacked, hasWindFuryAttacked, frozen, hasBeenUsed);
+	}
+	
+	@Override
+	public DeepCopyable deepCopy() {
+		return new Paladin(
+				this.name_, 
+				this.attack_,
+				this.extraAttackUntilTurnEnd_,
+				this.health_,
+				this.armor_,
+				this.weaponCharge_,
+				this.windFury_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.hasBeenUsed_
+				);
+	}
+	
+	/**
+	 * Use the hero ability on a given target
+	 * 
+	 * Paladin: summon a 1/1 Silver Hand Recruit
+	 * 
+	 * @param thisPlayerIndex The player index of the hero
+	 * @param targetPlayerIndex The player index of the target character
+	 * @param targetMinionIndex The minion index of the target character
+	 * @param boardState
+	 * @param deck
+	 * @return
+	 */
+	@Override
+	public HearthTreeNode<BoardState> useHeroAbility(
+			int thisPlayerIndex,
+			int targetPlayerIndex,
+			int targetMinionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSException
+	{
+		if (boardState.data_.getNumMinions_p0() >= 7)
+			return null;
+
+		HearthTreeNode<BoardState> toRet = super.useHeroAbility(thisPlayerIndex, targetPlayerIndex, targetMinionIndex, boardState, deck);
+
+		if (toRet != null) {
+			if (targetMinionIndex == 0 && targetPlayerIndex == 0) {
+				Minion theRecruit = new SilverHandRecruit();
+				boardState.data_.placeCard_hand(thisPlayerIndex, theRecruit);
+				//HORRID HACK
+				boardState.data_.setMana_p0(boardState.data_.getMana_p0() + 1);
+				theRecruit.useOn(boardState.data_.getNumCards_hand_p0() - 1, 0, boardState.data_.getNumMinions_p0() + 1, boardState, deck);
+			} else {
+				return null;
+			}
+		}
+		return toRet;
+	}
+}
---
+++ test/com/hearthsim/test/heroes/TestPaladin.java
@@ -0,0 +1,168 @@
+package com.hearthsim.test.heroes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Hero;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.concrete.BoulderfistOgre;
+import com.hearthsim.card.minion.concrete.RaidLeader;
+import com.hearthsim.card.minion.heroes.Paladin;
+import com.hearthsim.card.spellcard.concrete.TheCoin;
+import com.hearthsim.card.spellcard.concrete.WildGrowth;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class TestPaladin {
+
+
+	private HearthTreeNode<BoardState> board;
+	private Deck deck;
+
+	@Before
+	public void setup() {
+		board = new HearthTreeNode<BoardState>(new BoardState(new Paladin(), new Hero()));
+
+		Minion minion0_0 = new BoulderfistOgre();
+		Minion minion0_1 = new RaidLeader();
+		Minion minion1_0 = new BoulderfistOgre();
+		Minion minion1_1 = new RaidLeader();
+		
+		board.data_.placeCard_hand_p0(minion0_0);
+		board.data_.placeCard_hand_p0(minion0_1);
+				
+		board.data_.placeCard_hand_p1(minion1_0);
+		board.data_.placeCard_hand_p1(minion1_1);
+
+		Card cards[] = new Card[10];
+		for (int index = 0; index < 10; ++index) {
+			cards[index] = new TheCoin();
+		}
+	
+		deck = new Deck(cards);
+
+		Card fb = new WildGrowth();
+		board.data_.placeCard_hand_p0(fb);
+
+		board.data_.setMana_p0((byte)8);
+		board.data_.setMana_p1((byte)8);
+		
+		board.data_.setMaxMana_p0((byte)8);
+		board.data_.setMaxMana_p1((byte)8);
+		
+		HearthTreeNode<BoardState> tmpBoard = new HearthTreeNode<BoardState>(board.data_.flipPlayers());
+		try {
+			tmpBoard.data_.getCard_hand_p0(0).useOn(0, 0, 1, tmpBoard, deck);
+			tmpBoard.data_.getCard_hand_p0(0).useOn(0, 0, 1, tmpBoard, deck);
+		} catch (HSInvalidPlayerIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board = new HearthTreeNode<BoardState>(tmpBoard.data_.flipPlayers());
+		try {
+			board.data_.getCard_hand_p0(0).useOn(0, 0, 1, board, deck);
+			board.data_.getCard_hand_p0(0).useOn(0, 0, 1, board, deck);
+		} catch (HSInvalidPlayerIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.data_.resetMana();
+		board.data_.resetMinions();
+		
+	}
+	
+	@Test
+	public void test0() throws HSException {
+		
+		//null case
+		
+		Minion minion = board.data_.getMinion_p0(0);
+		HearthTreeNode<BoardState> ret = minion.attack(0, 1, 0, board, deck);
+		
+		assertFalse(ret == null);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 8);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 7);
+
+		assertEquals(board.data_.getMinion_p0(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getAttack(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getAttack(), 7);
+		
+		
+		Hero hero = board.data_.getHero_p0();
+		ret = hero.useHeroAbility(0, 1, 0, board, deck);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 6);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 7);
+
+		assertEquals(board.data_.getMinion_p0(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getAttack(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getAttack(), 7);
+		
+		minion.hasAttacked(false);
+		ret = minion.attack(0, 1, 2, board, deck);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 6);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 0);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 5);
+
+		assertEquals(board.data_.getMinion_p0(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getAttack(), 6);
+		assertEquals(board.data_.getMinion_p1(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getAttack(), 7);
+		
+		minion.hasAttacked(false);
+		ret = hero.useHeroAbility(0, 0, 0, board, deck);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 3);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 4);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 0);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p0(2).getHealth(), 1);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 5);
+
+		assertEquals(board.data_.getMinion_p0(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getAttack(), 6);
+		assertEquals(board.data_.getMinion_p0(2).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(0).getAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getAttack(), 7);
+		
+	}
+}
