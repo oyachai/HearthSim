c0e52db2184b3879827b7f971d4a07081906e541
oyachai <oyachai@gmail.com>
2014-07-15 23:57:29 -0400

    code clean up

--- src/com/hearthsim/card/minion/concrete/ArgentSquire.java
+++ src/com/hearthsim/card/minion/concrete/ArgentSquire.java
@@ -1,10 +1,6 @@
 package com.hearthsim.card.minion.concrete;
 
-import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Minion;
-import com.hearthsim.exception.HSInvalidPlayerIndexException;
-import com.hearthsim.util.BoardState;
-import com.hearthsim.util.HearthTreeNode;
 
 public class ArgentSquire extends Minion {
 	
