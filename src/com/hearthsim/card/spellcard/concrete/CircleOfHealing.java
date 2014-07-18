7099f05d95d893c689ebc17795d0f942b48b7652
oyachai <oyachai@gmail.com>
2014-07-18 11:18:52 -0400

    implemented Circle of Healing

---
+++ src/com/hearthsim/card/spellcard/concrete/CircleOfHealing.java
@@ -0,0 +1,77 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import java.util.Iterator;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.exception.HSInvalidPlayerIndexException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.HearthTreeNode;
+
+public class CircleOfHealing extends SpellCard {
+
+	private static final byte HEAL_AMOUNT = 4;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param hasBeenUsed Whether the card has already been used or not
+	 */
+	public CircleOfHealing(boolean hasBeenUsed) {
+		super("Circle of Healing", (byte)0, hasBeenUsed);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Defaults to hasBeenUsed = false
+	 */
+	public CircleOfHealing() {
+		this(false);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new CircleOfHealing(this.hasBeenUsed_);
+	}
+
+	
+	/**
+	 * 
+	 * Circle of Healing
+	 * 
+	 * Heals all minions for 4
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	protected HearthTreeNode<BoardState> use_core(
+			int thisCardIndex,
+			int playerIndex,
+			int minionIndex,
+			HearthTreeNode<BoardState> boardState,
+			Deck deck)
+		throws HSInvalidPlayerIndexException
+	{
+		if (playerIndex > 0 || minionIndex > 0) 
+			return null;
+		
+		for (int indx = 0; indx < boardState.data_.getNumMinions_p0(); ++indx) {
+			Minion targetMinion = boardState.data_.getMinion_p0(indx);
+			targetMinion.takeHeal(HEAL_AMOUNT, 0, indx + 1, boardState, deck);
+		}
+
+		for (int indx = 0; indx < boardState.data_.getNumMinions_p1(); ++indx) {
+			Minion targetMinion = boardState.data_.getMinion_p1(indx);
+			targetMinion.takeHeal(HEAL_AMOUNT, 1, indx + 1, boardState, deck);
+		}
+
+		return super.use_core(thisCardIndex, playerIndex, minionIndex, boardState, deck);
+	}
+}
