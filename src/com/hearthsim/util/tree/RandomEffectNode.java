b069758ddb665fb7babc3093b21a80bff881956a
oyachai <oyachai@hiroakis-pro.home>
2014-07-19 23:26:48 -0400

    RandomEffectNode

---
+++ src/com/hearthsim/util/tree/RandomEffectNode.java
@@ -0,0 +1,22 @@
+package com.hearthsim.util.tree;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.minion.Minion;
+
+public class RandomEffectNode extends StopNode {
+
+	public RandomEffectNode(HearthTreeNode origNode, Card cardUsed, int usedCardPlayerIndex, int usedCardIndex, int targetPlayerIndex, int targetMinionIndex) {
+		super(origNode, cardUsed, usedCardPlayerIndex, usedCardIndex, targetPlayerIndex, targetMinionIndex);
+	}
+	
+	public RandomEffectNode(HearthTreeNode origNode, Minion minionAttackedWith, int usedCardPlayerIndex, int usedCardIndex, int targetPlayerIndex, int targetMinionIndex) {
+		super(origNode, minionAttackedWith, usedCardPlayerIndex, usedCardIndex, targetPlayerIndex, targetMinionIndex);
+	}
+	
+	@Override
+	public double getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
