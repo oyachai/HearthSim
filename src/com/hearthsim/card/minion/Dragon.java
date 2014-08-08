d33c2ab277756a0643d12fb9101f46e705c68368
oyachai <oyachai@hiroakis-pro.home>
2014-08-07 23:41:32 -0400

    implemented Alexstrasza

---
+++ src/com/hearthsim/card/minion/Dragon.java
@@ -0,0 +1,78 @@
+package com.hearthsim.card.minion;
+
+import com.hearthsim.event.attack.AttackAction;
+import com.hearthsim.event.deathrattle.DeathrattleAction;
+import com.json.JSONObject;
+
+public class Dragon extends Minion {
+	
+public Dragon(
+		String name,
+		byte mana,
+		byte attack,
+		byte health,
+		byte baseAttack,
+		byte extraAttackUntilTurnEnd,
+		byte baseHealth,
+		byte maxHealth,
+		boolean taunt,
+		boolean divineShield,
+		boolean windFury,
+		boolean charge,
+		boolean hasAttacked,
+		boolean hasWindFuryAttacked,
+		boolean frozen,
+		boolean silenced,
+		boolean summoned,
+		boolean transformed,
+		boolean destroyOnTurnStart,
+		boolean destroyOnTurnEnd,
+		DeathrattleAction deathrattleAction,
+		AttackAction attackAction,
+		boolean isInHand,
+		boolean hasBeenUsed) {
+	super(
+			name,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			baseHealth,
+			maxHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			silenced,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			deathrattleAction,
+			attackAction,
+			isInHand,
+			hasBeenUsed);
+}
+
+public Dragon(
+		String name,
+		byte mana,
+		byte attack,
+		byte health,
+		byte baseAttack,
+		byte baseHealth,
+		byte maxHealth
+		) {
+	this(name, mana, attack, health, baseAttack, (byte)0, baseHealth, maxHealth, false, false, false, false, false, false, false, false, false, false, false, false, null, null, true, false);
+}
+
+public JSONObject toJSON() {
+	JSONObject json = super.toJSON();
+	json.put("type", "Beast");
+	return json;
+}
+}
---
+++ src/com/hearthsim/card/minion/concrete/Alexstrasza.java
@@ -0,0 +1,174 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Dragon;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.event.attack.AttackAction;
+import com.hearthsim.event.deathrattle.DeathrattleAction;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class Alexstrasza extends Dragon {
+
+	private static final String NAME = "Alexstrasza";
+	private static final byte MANA_COST = 9;
+	private static final byte ATTACK = 8;
+	private static final byte HEALTH = 8;
+	
+	private static final boolean TAUNT = false;
+	private static final boolean DIVINE_SHIELD = false;
+	private static final boolean WINDFURY = false;
+	private static final boolean CHARGE = false;
+	
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	
+	public Alexstrasza() {
+		this(
+				MANA_COST,
+				ATTACK,
+				HEALTH,
+				ATTACK,
+				(byte)0,
+				HEALTH,
+				HEALTH,
+				TAUNT,
+				DIVINE_SHIELD,
+				WINDFURY,
+				CHARGE,
+				false,
+				false,
+				false,
+				false,
+				SUMMONED,
+				TRANSFORMED,
+				false,
+				false,
+				null,
+				null,
+				true,
+				false
+			);
+	}
+	
+	public Alexstrasza(	
+			byte mana,
+			byte attack,
+			byte health,
+			byte baseAttack,
+			byte extraAttackUntilTurnEnd,
+			byte baseHealth,
+			byte maxHealth,
+			boolean taunt,
+			boolean divineShield,
+			boolean windFury,
+			boolean charge,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean silenced,
+			boolean summoned,
+			boolean transformed,
+			boolean destroyOnTurnStart,
+			boolean destroyOnTurnEnd,
+			DeathrattleAction deathrattleAction,
+			AttackAction attackAction,
+			boolean isInHand,
+			boolean hasBeenUsed) {
+		
+		super(
+			NAME,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			baseHealth,
+			maxHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			silenced,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			deathrattleAction,
+			attackAction,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new Alexstrasza(
+				this.mana_,
+				this.attack_,
+				this.health_,
+				this.baseAttack_,
+				this.extraAttackUntilTurnEnd_,
+				this.baseHealth_,
+				this.maxHealth_,
+				this.taunt_,
+				this.divineShield_,
+				this.windFury_,
+				this.charge_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.silenced_,
+				this.summoned_,
+				this.transformed_,
+				this.destroyOnTurnStart_,
+				this.destroyOnTurnEnd_,
+				this.deathrattleAction_,
+				this.attackAction_,
+				this.isInHand_,
+				this.hasBeenUsed_);
+	}
+
+	/**
+	 * 
+	 * Override for battlecry
+	 * 
+	 * Battlecry: Set a hero's remaining health to 15
+	 * 
+	 * @param thisCardIndex The index (position) of the card in the hand
+	 * @param playerIndex The index of the target player.  0 if targeting yourself or your own minions, 1 if targeting the enemy
+	 * @param minionIndex The index of the target minion.
+	 * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
+	 * 
+	 * @return The boardState is manipulated and returned
+	 */
+	@Override
+	public HearthTreeNode use_core(
+			int targetPlayerIndex,
+			Minion targetMinion,
+			HearthTreeNode boardState,
+			Deck deckPlayer0,
+			Deck deckPlayer1)
+		throws HSException
+	{
+		//A generic card does nothing except for consuming mana
+		HearthTreeNode toRet = super.use_core(targetPlayerIndex, targetMinion, boardState, deckPlayer0, deckPlayer1);
+		
+		if (toRet != null) {
+			{
+				HearthTreeNode newState = toRet.addChild(new HearthTreeNode((BoardState)toRet.data_.deepCopy()));
+				newState.data_.getHero_p0().setHealth((byte)15);
+			}
+			{
+				HearthTreeNode newState = toRet.addChild(new HearthTreeNode((BoardState)toRet.data_.deepCopy()));
+				newState.data_.getHero_p1().setHealth((byte)15);
+			}
+			return toRet;
+		} else {
+			return null;
+		}
+	}
+}
