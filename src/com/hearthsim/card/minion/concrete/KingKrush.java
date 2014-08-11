b2990c89e64103fb331fe2bbe833b9d962a613f0
oyachai <oyachai@gmail.com>
2014-08-11 16:20:02 -0400

    implemented King Krush

---
+++ src/com/hearthsim/card/minion/concrete/KingKrush.java
@@ -0,0 +1,137 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Beast;
+import com.hearthsim.event.attack.AttackAction;
+import com.hearthsim.event.deathrattle.DeathrattleAction;
+
+public class KingKrush extends Beast {
+
+	private static final String NAME = "King Krush";
+	private static final byte MANA_COST = 9;
+	private static final byte ATTACK = 8;
+	private static final byte HEALTH = 8;
+	
+	private static final boolean TAUNT = false;
+	private static final boolean DIVINE_SHIELD = false;
+	private static final boolean WINDFURY = false;
+	private static final boolean CHARGE = true;
+	
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	
+	public KingKrush() {
+		this(
+				MANA_COST,
+				ATTACK,
+				HEALTH,
+				ATTACK,
+				(byte)0,
+				(byte)0,
+				HEALTH,
+				HEALTH,
+				(byte)0,
+				TAUNT,
+				DIVINE_SHIELD,
+				WINDFURY,
+				CHARGE,
+				false,
+				false,
+				false,
+				false,
+				SUMMONED,
+				TRANSFORMED,
+				false,
+				false,
+				null,
+				null,
+				true,
+				false
+			);
+	}
+	
+	public KingKrush(	
+			byte mana,
+			byte attack,
+			byte health,
+			byte baseAttack,
+			byte extraAttackUntilTurnEnd,
+			byte auraAttack,
+			byte baseHealth,
+			byte maxHealth,
+			byte auraHealth,
+			boolean taunt,
+			boolean divineShield,
+			boolean windFury,
+			boolean charge,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean silenced,
+			boolean summoned,
+			boolean transformed,
+			boolean destroyOnTurnStart,
+			boolean destroyOnTurnEnd,
+			DeathrattleAction deathrattleAction,
+			AttackAction attackAction,
+			boolean isInHand,
+			boolean hasBeenUsed) {
+		
+		super(
+			NAME,
+			mana,
+			attack,
+			health,
+			baseAttack,
+			extraAttackUntilTurnEnd,
+			auraAttack,
+			baseHealth,
+			maxHealth,
+			auraHealth,
+			taunt,
+			divineShield,
+			windFury,
+			charge,
+			hasAttacked,
+			hasWindFuryAttacked,
+			frozen,
+			silenced,
+			summoned,
+			transformed,
+			destroyOnTurnStart,
+			destroyOnTurnEnd,
+			deathrattleAction,
+			attackAction,
+			isInHand,
+			hasBeenUsed);
+	}
+	
+	@Override
+	public Object deepCopy() {
+		return new KingKrush(
+				this.mana_,
+				this.attack_,
+				this.health_,
+				this.baseAttack_,
+				this.extraAttackUntilTurnEnd_,
+				this.auraAttack_,
+				this.baseHealth_,
+				this.maxHealth_,
+				this.auraHealth_,
+				this.taunt_,
+				this.divineShield_,
+				this.windFury_,
+				this.charge_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.silenced_,
+				this.summoned_,
+				this.transformed_,
+				this.destroyOnTurnStart_,
+				this.destroyOnTurnEnd_,
+				this.deathrattleAction_,
+				this.attackAction_,
+				this.isInHand_,
+				this.hasBeenUsed_);
+	}
+}
