61b1af1d1cb3e48aa54e6d416bf3ec87ccf699f5
oyachai <oyachai@gmail.com>
2014-08-18 00:53:44 -0400

    implemented Warlock hero class

---
+++ src/com/hearthsim/card/minion/heroes/Warlock.java
@@ -0,0 +1,84 @@
+package com.hearthsim.card.minion.heroes;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Hero;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.util.DeepCopyable;
+import com.hearthsim.util.tree.CardDrawNode;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class Warlock extends Hero {
+	
+	public Warlock() {
+		this("Warlock", (byte)30);
+	}
+
+	public Warlock(String name, byte health) {
+		this(name, (byte)0, (byte)0, health, (byte)0, (byte)0, false, false, false, false, false);
+	}
+	
+	public Warlock(
+			String name,
+			byte attack,
+			byte extraAttackUntilTurnEnd,
+			byte health,
+			byte armor,
+			byte weaponCharge,
+			boolean windFury,
+			boolean hasAttacked,
+			boolean hasWindFuryAttacked,
+			boolean frozen,
+			boolean hasBeenUsed) {
+	
+		super(name, attack, extraAttackUntilTurnEnd, health, armor, weaponCharge, windFury, hasAttacked, hasWindFuryAttacked, frozen, hasBeenUsed);
+	}
+	
+	@Override
+	public DeepCopyable deepCopy() {
+		return new Warlock(
+				this.name_, 
+				this.attack_,
+				this.extraAttackUntilTurnEnd_,
+				this.health_,
+				this.armor_,
+				this.weaponCharge_,
+				this.windFury_,
+				this.hasAttacked_,
+				this.hasWindFuryAttacked_,
+				this.frozen_,
+				this.hasBeenUsed_
+				);
+	}
+	
+	/**
+	 * Use the hero ability on a given target
+	 * 
+	 * Warlock: draw a card and take 2 damage
+	 * 
+	 * @param targetPlayerIndex The player index of the target character
+	 * @param targetMinion The target minion
+	 * @param boardState
+	 * @param deckPlayer0
+	 * @param deckPlayer1
+	 * 
+	 * @return
+	 */
+	@Override
+	public HearthTreeNode useHeroAbility_core(
+			int targetPlayerIndex,
+			Minion targetMinion,
+			HearthTreeNode boardState,
+			Deck deckPlayer0,
+			Deck deckPlayer1)
+		throws HSException
+	{
+		if (targetPlayerIndex != 0 || !(targetMinion instanceof Hero))
+			return null;
+		HearthTreeNode toRet = targetMinion.takeDamage((byte)2, 0, 0, boardState, deckPlayer0, deckPlayer1, false, false);
+		if (toRet != null) {
+			toRet = new CardDrawNode(toRet, 1);
+		}
+		return toRet;
+	}
+}
---
+++ test/com/hearthsim/test/heroes/TestWarlock.java
@@ -0,0 +1,132 @@
+package com.hearthsim.test.heroes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Hero;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.concrete.BoulderfistOgre;
+import com.hearthsim.card.minion.concrete.ChillwindYeti;
+import com.hearthsim.card.minion.concrete.RaidLeader;
+import com.hearthsim.card.minion.heroes.Warlock;
+import com.hearthsim.card.spellcard.concrete.TheCoin;
+import com.hearthsim.card.spellcard.concrete.WildGrowth;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.util.BoardState;
+import com.hearthsim.util.tree.CardDrawNode;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class TestWarlock {
+
+	private HearthTreeNode board;
+	private Deck deck;
+
+
+	@Before
+	public void setup() {
+		board = new HearthTreeNode(new BoardState(new Warlock(), new Hero()));
+
+		Minion minion0_0 = new BoulderfistOgre();
+		Minion minion0_1 = new RaidLeader();
+		Minion minion1_0 = new BoulderfistOgre();
+		Minion minion1_1 = new RaidLeader();
+		
+		board.data_.placeCard_hand_p0(minion0_0);
+		board.data_.placeCard_hand_p0(minion0_1);
+				
+		board.data_.placeCard_hand_p1(minion1_0);
+		board.data_.placeCard_hand_p1(minion1_1);
+
+		Card cards[] = new Card[10];
+		for (int index = 0; index < 10; ++index) {
+			cards[index] = new TheCoin();
+		}
+	
+		deck = new Deck(cards);
+
+		Card fb = new WildGrowth();
+		board.data_.placeCard_hand_p0(fb);
+
+		board.data_.setMana_p0((byte)8);
+		board.data_.setMana_p1((byte)8);
+		
+		board.data_.setMaxMana_p0((byte)8);
+		board.data_.setMaxMana_p1((byte)8);
+		
+		HearthTreeNode tmpBoard = new HearthTreeNode(board.data_.flipPlayers());
+		try {
+			tmpBoard.data_.getCard_hand_p0(0).useOn(0, tmpBoard.data_.getHero_p0(), tmpBoard, deck, null);
+			tmpBoard.data_.getCard_hand_p0(0).useOn(0, tmpBoard.data_.getHero_p0(), tmpBoard, deck, null);
+		} catch (HSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board = new HearthTreeNode(tmpBoard.data_.flipPlayers());
+		try {
+			board.data_.getCard_hand_p0(0).useOn(0, board.data_.getHero_p0(), board, deck, null);
+			board.data_.getCard_hand_p0(0).useOn(0, board.data_.getHero_p0(), board, deck, null);
+		} catch (HSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.data_.resetMana();
+		board.data_.resetMinions();
+		
+	}
+	
+	@Test
+	public void test0() throws HSException {
+		
+		Minion target = board.data_.getCharacter(1, 0);
+		Minion minion = board.data_.getMinion_p0(0);
+		HearthTreeNode ret = minion.attack(1, target, board, deck, null);
+		
+		assertFalse(ret == null);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 8);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 30);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 7);
+
+		assertEquals(board.data_.getMinion_p0(0).getTotalAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getTotalAttack(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getTotalAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getTotalAttack(), 7);
+		
+		
+		target = board.data_.getCharacter(0, 0);
+		Hero hero = board.data_.getHero_p0();
+		ret = hero.useHeroAbility(0, target, board, deck, null);
+		assertEquals(board.data_.getNumCards_hand(), 1);
+		
+		assertTrue(ret instanceof CardDrawNode);
+		assertEquals(((CardDrawNode)ret).getNumCardsToDraw(), 1);
+		
+		assertEquals(board.data_.getNumMinions_p0(), 2);
+		assertEquals(board.data_.getNumMinions_p1(), 2);
+		assertEquals(board.data_.getMana_p0(), 6);
+		assertEquals(board.data_.getMana_p1(), 8);
+		assertEquals(board.data_.getHero_p0().getHealth(), 28);
+		assertEquals(board.data_.getHero_p1().getHealth(), 28);
+		assertEquals(board.data_.getMinion_p0(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getHealth(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getHealth(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getHealth(), 7);
+
+		assertEquals(board.data_.getMinion_p0(0).getTotalAttack(), 2);
+		assertEquals(board.data_.getMinion_p0(1).getTotalAttack(), 7);
+		assertEquals(board.data_.getMinion_p1(0).getTotalAttack(), 2);
+		assertEquals(board.data_.getMinion_p1(1).getTotalAttack(), 7);
+		
+	}
+}
