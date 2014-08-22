9c5e63c70a57154a20fc7826c9a611dac39d5a95
kallin <kallin.nagelberg@gmail.com>
2014-08-22 13:34:33 -0400

    restructuring project for gradle build

---
+++ src/main/java/com/hearthsim/GameRecord.java
@@ -0,0 +1,48 @@
+package com.hearthsim;
+
+import java.util.ArrayList;
+
+import com.hearthsim.util.BoardState;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GameRecord {
+	
+	ArrayList<ArrayList<BoardState>> boards_;
+	
+	public GameRecord() {
+		boards_ = new ArrayList<ArrayList<BoardState>>(2);
+		boards_.add(new ArrayList<BoardState>());
+		boards_.add(new ArrayList<BoardState>());
+	}
+	
+	public void put(int playerID, BoardState board) {
+		boards_.get(playerID).add(board);
+	}
+	
+	public BoardState get(int turn, int playerID) {
+		return boards_.get(playerID).get(turn);
+	}
+
+	public int getRecordLength(int playerID) {
+		return boards_.get(playerID).size();
+	}
+	
+	public JSONObject toJSON() {
+		JSONObject json = new JSONObject();
+		
+		JSONArray b0 = new JSONArray();
+		for (BoardState board : boards_.get(0)) {
+			b0.put(board.toJSON());
+		}
+
+		JSONArray b1 = new JSONArray();
+		for (BoardState board : boards_.get(1)) {
+			b1.put(board.toJSON());
+		}
+		
+		json.put("p0", b0);
+		json.put("p1", b1);
+		return json;
+	}
+}
