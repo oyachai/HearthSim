d34b93b4d2eabd6b4e6684024df034297bf3eb1f
Adam Babcock <github@mrhen.com>
2014-11-12 11:09:59 -0600

    Converted FanOfKnives into SpellDamageAoe and removed resulting duplicate tests.

--- src/main/java/com/hearthsim/card/spellcard/concrete/FanOfKnives.java
+++ src/main/java/com/hearthsim/card/spellcard/concrete/FanOfKnives.java
@@ -2,14 +2,15 @@ package com.hearthsim.card.spellcard.concrete;
 
 import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Minion;
-import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.card.spellcard.SpellDamageAoe;
 import com.hearthsim.exception.HSException;
 import com.hearthsim.model.PlayerSide;
 import com.hearthsim.util.tree.CardDrawNode;
 import com.hearthsim.util.tree.HearthTreeNode;
 
-public class FanOfKnives extends SpellCard {
+public class FanOfKnives extends SpellDamageAoe {
 
+	private static final byte DAMAGE_AMOUNT = 1;
 
 	/**
 	 * Constructor
@@ -17,64 +18,37 @@ public class FanOfKnives extends SpellCard {
 	 * @param hasBeenUsed Whether the card has already been used or not
 	 */
 	public FanOfKnives(boolean hasBeenUsed) {
-		super((byte)3, hasBeenUsed);
+		super((byte)3, DAMAGE_AMOUNT, hasBeenUsed);
 	}
 
 	/**
 	 * Constructor
-	 * 
 	 * Defaults to hasBeenUsed = false
 	 */
 	public FanOfKnives() {
 		this(false);
 	}
 
-	@Override
-	public SpellCard deepCopy() {
-		return new FanOfKnives(this.hasBeenUsed);
-	}
-
 	/**
-	 * 
 	 * Use the card on the given target
-	 * 
 	 * This card damages all enemy minions by 1 and draws a card
 	 * 
-	 *
-     *
-     * @param side
-     * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
-     *
-     * @return The boardState is manipulated and returned
+	 * @param side
+	 * @param boardState The BoardState before this card has performed its action. It will be manipulated and returned.
+	 * @return The boardState is manipulated and returned
 	 */
 	@Override
-	protected HearthTreeNode use_core(
-			PlayerSide side,
-			Minion targetMinion,
-			HearthTreeNode boardState,
-			Deck deckPlayer0,
-			Deck deckPlayer1,
-			boolean singleRealizationOnly)
-		throws HSException
-	{
-		if (isCurrentPlayer(side)) {
-			return null;
-		}
-		
-		if (isNotHero(targetMinion)) {
-			return null;
-		}
-		
-		HearthTreeNode toRet = super.use_core(side, targetMinion, boardState, deckPlayer0, deckPlayer1, singleRealizationOnly);
-		if (toRet != null) {
-			for (Minion minion : PlayerSide.WAITING_PLAYER.getPlayer(toRet).getMinions()) {
-				toRet = minion.takeDamage((byte)1, PlayerSide.CURRENT_PLAYER, PlayerSide.WAITING_PLAYER, toRet, deckPlayer0, deckPlayer1, true, false);
-			}
-			
-			if (toRet instanceof CardDrawNode) {
-				((CardDrawNode) toRet).addNumCardsToDraw(1);
+	protected HearthTreeNode use_core(PlayerSide side, Minion targetMinion, HearthTreeNode boardState,
+			Deck deckPlayer0, Deck deckPlayer1, boolean singleRealizationOnly) throws HSException {
+
+		HearthTreeNode toRet = super.use_core(side, targetMinion, boardState, deckPlayer0, deckPlayer1,
+				singleRealizationOnly);
+
+		if(toRet != null) {
+			if(toRet instanceof CardDrawNode) {
+				((CardDrawNode)toRet).addNumCardsToDraw(1);
 			} else {
-				toRet = new CardDrawNode(toRet, 1); //draw a card
+				toRet = new CardDrawNode(toRet, 1); // draw a card
 			}
 		}
 		return toRet;
