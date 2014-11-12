93f84f0023aeee5de11372c7f29a63d6d53ba47d
Adam Babcock <github@mrhen.com>
2014-11-12 09:29:36 -0600

    Close resource

--- src/main/java/com/ptplot/FileUtilities.java
+++ src/main/java/com/ptplot/FileUtilities.java
@@ -126,19 +126,23 @@ public class FileUtilities {
     public static void extractJarFile(String jarFileName, String directoryName)
             throws IOException {
         JarFile jarFile = new JarFile(jarFileName);
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry jarEntry = (JarEntry) entries.nextElement();
-            File destinationFile = new File(directoryName, jarEntry.getName());
-            if (jarEntry.isDirectory()) {
-                if (!destinationFile.isDirectory() && !destinationFile.mkdirs()) {
-                    throw new IOException("Warning, failed to create "
-                            + "directory for \"" + destinationFile + "\".");
-                }
-            } else {
-                _binaryCopyStream(jarFile.getInputStream(jarEntry),
-                        destinationFile);
-            }
+        try {
+	        Enumeration<JarEntry> entries = jarFile.entries();
+	        while (entries.hasMoreElements()) {
+	            JarEntry jarEntry = (JarEntry) entries.nextElement();
+	            File destinationFile = new File(directoryName, jarEntry.getName());
+	            if (jarEntry.isDirectory()) {
+	                if (!destinationFile.isDirectory() && !destinationFile.mkdirs()) {
+	                    throw new IOException("Warning, failed to create "
+	                            + "directory for \"" + destinationFile + "\".");
+	                }
+	            } else {
+	                _binaryCopyStream(jarFile.getInputStream(jarEntry),
+	                        destinationFile);
+	            }
+	        }
+        } finally {
+        	jarFile.close();
         }
     }
 
