aa226cc762a31f49b2332c0f36c5792f6cbc0dd0
oyachai <oyachai@gmail.com>
2014-11-18 00:46:37 -0500

    Implemented Master Swordsmith

---
+++ src/main/java/com/hearthsim/card/minion/concrete/MasterSwordsmith.java
@@ -0,0 +1,38 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class MasterSwordsmith extends Minion {
+
+	private static final boolean HERO_TARGETABLE = true;
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	private static final byte SPELL_DAMAGE = 5;
+	
+	public MasterSwordsmith() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+        summoned_ = SUMMONED;
+        transformed_ = TRANSFORMED;
+	}
+	
+	@Override
+	public HearthTreeNode endTurn(PlayerSide thisMinionPlayerIndex, HearthTreeNode boardModel, Deck deckPlayer0, Deck deckPlayer1) throws HSException {
+		HearthTreeNode toRet = boardModel;
+		int numMinions = PlayerSide.CURRENT_PLAYER.getPlayer(toRet).getNumMinions();
+		if (thisMinionPlayerIndex == PlayerSide.CURRENT_PLAYER && numMinions > 1) {	
+			Minion buffTargetMinion = PlayerSide.CURRENT_PLAYER.getPlayer(toRet).getMinions().get((int)(Math.random() * numMinions));
+			while (buffTargetMinion == this) {
+				buffTargetMinion = PlayerSide.CURRENT_PLAYER.getPlayer(toRet).getMinions().get((int)(Math.random() * numMinions));
+			}
+			buffTargetMinion.addAttack((byte)1);
+		}
+		return super.endTurn(thisMinionPlayerIndex, toRet, deckPlayer0, deckPlayer1);
+	}
+
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/MasterSwordsmithSpec.groovy
@@ -0,0 +1,72 @@
+package com.hearthsim.test.groovy.card
+
+
+import com.hearthsim.card.minion.concrete.MasterSwordsmith
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+class MasterSwordsmithSpec extends CardSpec {
+
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def setup() {
+
+		def minionMana = 2;
+		def attack = 5;
+		def health0 = 3;
+		def health1 = 7;
+
+		def commonField = [
+				[mana: minionMana, attack: attack, maxHealth: health0], //todo: attack may be irrelevant here
+		]
+
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([MasterSwordsmith])
+				field(commonField)
+				mana(7)
+			}
+			waitingPlayer {
+				field(commonField)
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+	}
+	
+	def "playing Master Swordsmith"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 1)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(MasterSwordsmith)
+				mana(5)
+			}
+		}
+
+		def retAfterEndTurn = new HearthTreeNode(Game.endTurn(ret.data_))
+		assertBoardDelta(copiedBoard, retAfterEndTurn.data_) {
+			currentPlayer {
+				playMinion(MasterSwordsmith)
+				mana(5)
+				updateMinion(0, [deltaAttack: 1])
+			}
+		}
+
+	}
+	
+}
