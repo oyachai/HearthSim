0b461ddd86df2f39d8906f9f28d3c6da48d94bb8
geoffrey-liu <geoffrey.p.liu@gmail.com>
2014-11-23 15:42:14 -0500

    BaseEntity class setup

---
+++ src/main/java/com/hearthsim/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.hearthsim.entity;
+
+public class BaseEntity
+{
+	private int health;
+	private int attack;
+	
+	public BaseEntity(int health, int attack)
+	{
+		this.health = health;
+		this.attack = attack;
+	}
+}
