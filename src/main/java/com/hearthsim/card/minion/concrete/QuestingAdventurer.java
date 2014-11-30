d2e7c97b6d7a6c1dab5ea51c9517e7865a0a48fc
oyachai <oyachai@PapaPanda.home>
2014-11-29 21:33:04 -0500

    implemented Questing Adventurer

---
+++ src/main/java/com/hearthsim/card/minion/concrete/QuestingAdventurer.java
@@ -0,0 +1,44 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class QuestingAdventurer extends Minion {
+
+	private static final boolean HERO_TARGETABLE = true;
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	private static final byte SPELL_DAMAGE = 0;
+	
+	public QuestingAdventurer() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+        summoned_ = SUMMONED;
+        transformed_ = TRANSFORMED;
+	}
+	
+	public HearthTreeNode onCardPlayBegin(
+			PlayerSide thisCardPlayerSide,
+			PlayerSide cardUserPlayerSide,
+			Card usedCard,
+			HearthTreeNode boardState,
+			Deck deckPlayer0,
+			Deck deckPlayer1,
+			boolean singleRealizationOnly)
+	throws HSException
+	{
+		HearthTreeNode toRet = super.onCardPlayBegin(thisCardPlayerSide, cardUserPlayerSide, usedCard, boardState, deckPlayer0, deckPlayer1, singleRealizationOnly);
+		if (usedCard != this && thisCardPlayerSide == cardUserPlayerSide) {
+			this.addAttack((byte)1);
+			this.addHealth((byte)1);
+			this.addMaxHealth((byte)1);
+		}
+		return toRet;
+	}
+
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/QuestingAdventurerSpec.groovy
@@ -0,0 +1,83 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.Card
+import com.hearthsim.card.Deck
+import com.hearthsim.card.minion.concrete.QuestingAdventurer
+import com.hearthsim.card.spellcard.concrete.TheCoin
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+
+class QuestingAdventurerSpec extends CardSpec {
+
+	def "playing QuestingAdventurer"() {
+		
+		def startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([QuestingAdventurer])
+				mana(9)
+			}
+			waitingPlayer {
+				mana(9)
+			}
+		}
+
+		def root = new HearthTreeNode(startingBoard)
+
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(QuestingAdventurer)
+				mana(6)
+			}
+		}
+
+	}
+	
+	def "playing a card with a Questing Adventurer on the field"() {
+		
+		def startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([TheCoin])
+				field([[minion: QuestingAdventurer]])
+				mana(9)
+			}
+			waitingPlayer {
+				field([[minion: QuestingAdventurer]]) //This Questing Adventurer should not be buffed
+				mana(9)
+			}
+		}
+
+		def root = new HearthTreeNode(startingBoard)
+
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				removeCardFromHand(TheCoin)
+				mana(10)
+				updateMinion(0, [deltaHealth: 1, deltaAttack: 1, deltaMaxHealth: 1])
+			}
+		}
+
+	}
+
+	
+}
