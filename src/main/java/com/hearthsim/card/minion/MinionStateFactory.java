8ec1ccd99afcdf9f71a7df77971182458a65f0d6
Robert Wen <robertwen20@gmail.com>
2014-12-01 17:53:24 -0500

    Created a MinionState factory so we can use that factory instead of always making a new MinionState object

---
+++ src/main/java/com/hearthsim/card/minion/MinionStateFactory.java
@@ -0,0 +1,19 @@
+package com.hearthsim.card.minion;
+
+public class MinionStateFactory {
+	
+	public MinionState makeFrozen(){
+		return new FrozenState();
+	}
+	
+	public MinionState makeStealthed(){
+		return new StealthedState();
+	}
+	
+	public MinionState makeCharge(){
+		return new ChargeState();
+	}
+	
+	
+
+}
