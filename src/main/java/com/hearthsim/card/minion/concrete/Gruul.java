15df597deb2a70a36c6ed8e5c05f31b25d6f8c3b
oyachai <oyachai@PapaPanda.home>
2014-12-11 21:56:57 -0500

    implemented Gruul

---
+++ src/main/java/com/hearthsim/card/minion/concrete/Gruul.java
@@ -0,0 +1,37 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class Gruul extends Minion {
+	
+	private static final boolean HERO_TARGETABLE = true;
+	private static final boolean SUMMONED = false;
+	private static final boolean TRANSFORMED = false;
+	private static final byte SPELL_DAMAGE = 0;
+	
+	public Gruul() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+        summoned_ = SUMMONED;
+        transformed_ = TRANSFORMED;
+	}
+	
+	/**
+	 * 
+	 * At the end of each turn, gain +1/+1
+	 * 
+	 */
+	@Override
+	public HearthTreeNode endTurn(PlayerSide thisMinionPlayerIndex, HearthTreeNode boardModel, Deck deckPlayer0, Deck deckPlayer1) throws HSException {
+		HearthTreeNode tmpState = super.endTurn(thisMinionPlayerIndex, boardModel, deckPlayer0, deckPlayer1);
+		this.addHealth((byte)1);
+		this.addMaxHealth((byte)1);
+		this.addAttack((byte)1);
+		return tmpState;
+	}
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/GruulSpec.groovy
@@ -0,0 +1,64 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.minion.concrete.Gruul
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+class GruulSpec extends CardSpec {
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def setup() {
+
+		def minionMana = 2;
+		def attack = 5;
+		def health0 = 3;
+		def health1 = 7;
+
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([Gruul])
+				mana(8)
+			}
+			waitingPlayer {
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+	}
+	
+	def "playing Gruul"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(Gruul)
+				mana(0)
+			}
+		}
+
+		def retAfterEndTurn = new HearthTreeNode(Game.endTurn(ret.data_))
+		assertBoardDelta(copiedBoard, retAfterEndTurn.data_) {
+			currentPlayer {
+				playMinion(Gruul)
+				mana(0)
+				updateMinion(0, [deltaHealth: 1, deltaAttack: 1, deltaMaxHealth: 1])
+			}
+		}
+
+	}
+	
+}
