f7447b62c2033c45bbf148190fb5f0fac44b647c
Adam Babcock <github@mrhen.com>
2014-12-14 15:11:24 -0600

    Remove unused method

--- src/main/java/com/hearthsim/card/minion/Minion.java
+++ src/main/java/com/hearthsim/card/minion/Minion.java
@@ -1118,23 +1118,6 @@ public class Minion extends Card implements CardEndTurnInterface, CardStartTurnI
 	// ======================================================================================
 
 	/**
-	 * Called whenever another minion is summoned, before the summoning
-	 *
-	 * @param thisMinionPlayerSide
-	 * @param summonedMinionPlayerSide
-	 * @param summonedMinion The summoned minion
-	 * @param boardState The BoardState before this card has performed its action. It will be manipulated and returned.
-	 * @param deckPlayer0 The deck of player0
-	 * @param deckPlayer1 The deck of player1
-	 * @return The boardState is manipulated and returned
-	 * */
-	public HearthTreeNode minionAfterSummonEvent(PlayerSide thisMinionPlayerSide, PlayerSide summonedMinionPlayerSide,
-			Minion summonedMinion, HearthTreeNode boardState, Deck deckPlayer0, Deck deckPlayer1)
-			throws HSInvalidPlayerIndexException {
-		return boardState;
-	}
-
-	/**
 	 * 
 	 * Called whenever another minion is attacking another character
 	 * 
