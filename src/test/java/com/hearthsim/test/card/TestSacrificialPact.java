f729cf444665dd54f2d77730f01dde6edd4f570b
Adam Babcock <github@mrhen.com>
2014-12-13 23:02:56 -0600

    Organized imports

--- src/test/java/com/hearthsim/test/card/TestSacrificialPact.java
+++ src/test/java/com/hearthsim/test/card/TestSacrificialPact.java
@@ -1,22 +1,23 @@
 package com.hearthsim.test.card;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import com.hearthsim.card.Card;
 import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Demon;
 import com.hearthsim.card.minion.Minion;
 import com.hearthsim.card.minion.concrete.BloodImp;
-import com.hearthsim.card.spellcard.concrete.Execute;
 import com.hearthsim.card.spellcard.concrete.SacrificialPact;
 import com.hearthsim.card.spellcard.concrete.TheCoin;
 import com.hearthsim.exception.HSException;
 import com.hearthsim.model.BoardModel;
 import com.hearthsim.model.PlayerSide;
 import com.hearthsim.util.tree.HearthTreeNode;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class TestSacrificialPact {
 
