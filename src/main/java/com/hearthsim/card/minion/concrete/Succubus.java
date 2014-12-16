50aeef20111791d3d4af5b0feaea017d6b8e246f
oyachai <oyachai@PapaPanda.home>
2014-12-15 23:23:40 -0500

    (re)Implemented Succubus

--- src/main/java/com/hearthsim/card/minion/concrete/Succubus.java
+++ src/main/java/com/hearthsim/card/minion/concrete/Succubus.java
@@ -1,11 +1,19 @@
 package com.hearthsim.card.minion.concrete;
 
+import java.util.EnumSet;
+
+import com.hearthsim.card.Card;
 import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Demon;
+import com.hearthsim.card.minion.Hero;
 import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.Minion.BattlecryTargetType;
 import com.hearthsim.exception.HSException;
 import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.HearthAction;
+import com.hearthsim.util.IdentityLinkedList;
 import com.hearthsim.util.tree.HearthTreeNode;
+import com.hearthsim.util.tree.RandomEffectNode;
 
 
 public class Succubus extends Demon {
@@ -20,38 +28,45 @@ public class Succubus extends Demon {
 
 	}
 	
+	@Override
+	public EnumSet<BattlecryTargetType> getBattlecryTargets() {
+		return EnumSet.of(BattlecryTargetType.NO_TARGET);
+	}
+	
 	/**
-	 * 
-	 * Override for battlecry
-	 * 
 	 * Battlecry: Discard a random card
-	 * 
-	 * INCOMPLETE IMPLEMENTATION!!!!!!!!!!!!!!
-     *
-     * @param side
-     * @param boardState The BoardState before this card has performed its action.  It will be manipulated and returned.
-     *
-     * @return The boardState is manipulated and returned
 	 */
 	@Override
-	public HearthTreeNode use_core(
-			PlayerSide side,
-			Minion targetMinion,
+	public HearthTreeNode useUntargetableBattlecry_core(
+			Minion minionPlacementTarget,
 			HearthTreeNode boardState,
 			Deck deckPlayer0,
 			Deck deckPlayer1,
-			boolean singleRealizationOnly)
-		throws HSException
+			boolean singleRealizationOnly
+		) throws HSException
 	{
-
-		HearthTreeNode toRet = super.use_core(side, targetMinion, boardState, deckPlayer0, deckPlayer1, singleRealizationOnly);
+		HearthTreeNode toRet = boardState;
 		
-		int numCards = toRet.data_.getNumCardsHandCurrentPlayer();
-		if (numCards <= 0)
-			return null;
-		int cardToDiscardIndex = (int)(Math.random() * numCards);
-		toRet.data_.removeCardFromHand(PlayerSide.CURRENT_PLAYER.getPlayer(toRet).getMinions().get(cardToDiscardIndex), PlayerSide.CURRENT_PLAYER);
-							
-		return boardState;
+		if (singleRealizationOnly) {
+			IdentityLinkedList<Card> hand = PlayerSide.CURRENT_PLAYER.getPlayer(toRet).getHand();
+			if (hand.size() > 0) {
+				Card targetCard = hand.get((int)(Math.random() * hand.size()));
+				toRet.data_.removeCard_hand(targetCard);
+			}			
+		} else {
+			int placementTargetIndex = minionPlacementTarget instanceof Hero ? 0 : PlayerSide.CURRENT_PLAYER.getPlayer(boardState).getMinions().indexOf(minionPlacementTarget) + 1;
+			int thisMinionIndex = PlayerSide.CURRENT_PLAYER.getPlayer(boardState).getMinions().indexOf(this) + 1;
+			IdentityLinkedList<Card> hand = toRet.data_.getCurrentPlayerHand();
+			if (hand.size() == 0) {
+				return toRet;
+			}
+			toRet = new RandomEffectNode(boardState, new HearthAction(HearthAction.Verb.UNTARGETABLE_BATTLECRY, PlayerSide.CURRENT_PLAYER, thisMinionIndex, PlayerSide.CURRENT_PLAYER, placementTargetIndex));
+			for (int indx0 = 0; indx0 < hand.size(); ++indx0) {
+				HearthTreeNode cNode = new HearthTreeNode(toRet.data_.deepCopy());
+				cNode.data_.removeCard_hand(cNode.data_.getCurrentPlayerCardHand(indx0));					
+				toRet.addChild(cNode);
+			}
+		}
+		return toRet;		
 	}
 }
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/SuccubusSpec.groovy
@@ -0,0 +1,148 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.Card
+import com.hearthsim.card.Deck
+import com.hearthsim.card.minion.concrete.Succubus
+import com.hearthsim.card.spellcard.concrete.TheCoin
+import com.hearthsim.card.spellcard.concrete.HolySmite
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+class SuccubusSpec extends CardSpec {
+
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def "playing Succubus with no other cards in hand"() {
+
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([Succubus])
+				mana(7)
+			}
+			waitingPlayer {
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+			}
+		}
+	}
+
+	
+	def "playing Succubus with no one other card in hand"() {
+		
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([Succubus, TheCoin])
+				mana(7)
+			}
+			waitingPlayer {
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+			}
+		}
+		
+		assertEquals(ret.numChildren(), 1);
+		
+		HearthTreeNode child0 = ret.getChildren().get(0);
+		assertBoardDelta(copiedBoard, child0.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+				removeCardFromHand(TheCoin)
+			}
+		}
+
+	}
+		
+	
+	
+	
+	def "playing Succubus with two other cards in hand"() {
+		
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([Succubus, TheCoin, HolySmite])
+				mana(7)
+			}
+			waitingPlayer {
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+
+		def copiedBoard = startingBoard.deepCopy()
+		def target = root.data_.getCharacter(CURRENT_PLAYER, 0)
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, target, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+			}
+		}
+		
+		assertEquals(ret.numChildren(), 2);
+		
+		HearthTreeNode child0 = ret.getChildren().get(0);
+		assertBoardDelta(copiedBoard, child0.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+				removeCardFromHand(TheCoin)
+			}
+		}
+
+		HearthTreeNode child1 = ret.getChildren().get(1);
+		assertBoardDelta(copiedBoard, child1.data_) {
+			currentPlayer {
+				playMinion(Succubus)
+				mana(5)
+				removeCardFromHand(HolySmite)
+			}
+		}
+
+	}
+}
