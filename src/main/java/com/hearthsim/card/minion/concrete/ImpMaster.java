2379992cf8cc93352c59a924a18f5886ea210a09
oyachai <oyachai@PapaPanda.home>
2014-12-27 14:54:54 -0500

    Implemented Imp Master

---
+++ src/main/java/com/hearthsim/card/minion/concrete/ImpMaster.java
@@ -0,0 +1,33 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class ImpMaster extends Minion {
+	
+	private static final boolean HERO_TARGETABLE = true;
+	private static final byte SPELL_DAMAGE = 0;
+	
+	public ImpMaster() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+	}
+	
+	@Override
+	public HearthTreeNode endTurn(PlayerSide thisMinionPlayerIndex, HearthTreeNode boardModel, Deck deckPlayer0, Deck deckPlayer1) throws HSException {
+		HearthTreeNode tmpState = super.endTurn(thisMinionPlayerIndex, boardModel, deckPlayer0, deckPlayer1);
+		if (isWaitingPlayer(thisMinionPlayerIndex))
+			return tmpState;
+		
+		tmpState = this.takeDamage((byte)1, thisMinionPlayerIndex, thisMinionPlayerIndex, tmpState, deckPlayer0, deckPlayer1, false, false);
+		if (tmpState.data_.getCurrentPlayer().getMinions().size() < 7) {
+			tmpState = new Imp().summonMinion(thisMinionPlayerIndex, this, tmpState, deckPlayer0, deckPlayer1, false, true);
+		}
+		
+		return tmpState;
+	}
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/ImpMasterSpec.groovy
@@ -0,0 +1,87 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.minion.concrete.ImpMaster
+import com.hearthsim.card.minion.concrete.Imp
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+public class ImpMasterSpec extends CardSpec {
+
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def setup() {
+
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([ImpMaster])
+				mana(8)
+			}
+			waitingPlayer {
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+	}
+	
+	def "playing Imp Master"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, 0, root, null, null)
+
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(ImpMaster)
+				mana(5)
+			}
+		}
+
+		def retAfterEndTurn = new HearthTreeNode(Game.endTurn(ret.data_))
+		assertBoardDelta(copiedBoard, retAfterEndTurn.data_) {
+			currentPlayer {
+				playMinion(ImpMaster)
+				addMinionToField(Imp)
+				mana(5)
+				updateMinion(0, [deltaHealth: -1])
+			}
+		}
+	}
+	
+	def "playing Imp Master with 1 health left"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, 0, root, null, null)
+		theCard.health_ = 1
+		
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(ImpMaster)
+				updateMinion(0, [deltaHealth: -4])
+				mana(5)
+			}
+		}
+
+		def retAfterEndTurn = new HearthTreeNode(Game.endTurn(ret.data_))
+		assertBoardDelta(copiedBoard, retAfterEndTurn.data_) {
+			currentPlayer {
+				playMinion(ImpMaster)
+				removeMinion(0)
+				addMinionToField(Imp)
+				mana(5)
+			}
+		}
+	}
+}
