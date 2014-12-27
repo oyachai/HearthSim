63050fe54aa16d04950a4eb5778f2d73534d6e68
oyachai <oyachai@PapaPanda.home>
2014-12-27 15:08:47 -0500

    Implemented Lightspawn

---
+++ src/main/java/com/hearthsim/card/minion/concrete/Lightspawn.java
@@ -0,0 +1,20 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+public class Lightspawn extends Minion {
+
+	private static final boolean HERO_TARGETABLE = true;
+	private static final byte SPELL_DAMAGE = 0;
+	
+	public Lightspawn() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+	}
+	
+	@Override
+	public byte getTotalAttack() {
+		return this.getTotalHealth();
+	}
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/LightspawnSpec.groovy
@@ -0,0 +1,61 @@
+package com.hearthsim.test.groovy.card
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+import com.hearthsim.card.minion.concrete.Lightspawn
+import com.hearthsim.card.spellcard.concrete.ShadowWordDeath
+import com.hearthsim.model.BoardModel
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+class LightspawnSpec extends CardSpec {
+
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def setup() {
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([ShadowWordDeath])
+				mana(5)
+			}
+			waitingPlayer {
+				field([[minion: Lightspawn]])
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+	}
+	
+	def "Lightspawn is killed by a Shadow Word: Death"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(WAITING_PLAYER, 1, root, null, null)
+
+		expect:
+		assertEquals(root, ret);
+
+		assertBoardDelta(copiedBoard, root.data_) {
+			currentPlayer {
+				removeCardFromHand(ShadowWordDeath)
+				mana(2)
+			}
+			waitingPlayer {
+				removeMinion(0)
+			}
+		}
+	}
+	
+	def "Damaged Lightspawn is not killed by a Shadow Word: Death"() {
+		def copiedBoard = startingBoard.deepCopy()
+		root.data_.getWaitingPlayerCharacter(1).health_ = 4
+
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(WAITING_PLAYER, 1, root, null, null)
+
+		expect:
+		assertTrue(ret == null);
+	}
+}
