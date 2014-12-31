f056e015d0ebfb9fc77a0efee546cd56bd761f9d
oyachai <oyachai@PapaPanda.home>
2014-12-31 01:32:33 -0500

    Implemented Illidan Stormrage

---
+++ src/main/java/com/hearthsim/card/minion/concrete/FlameOfAzzinoth.java
@@ -0,0 +1,15 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+public class FlameOfAzzinoth extends Minion {
+
+	private static final boolean HERO_TARGETABLE = true;
+	private static final byte SPELL_DAMAGE = 0;
+	
+	public FlameOfAzzinoth() {
+        super();
+        spellDamage_ = SPELL_DAMAGE;
+        heroTargetable_ = HERO_TARGETABLE;
+	}
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/IllidanStormrageSpec.groovy
@@ -0,0 +1,65 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.minion.concrete.IllidanStormrage
+import com.hearthsim.card.minion.concrete.FlameOfAzzinoth
+import com.hearthsim.card.spellcard.concrete.TheCoin
+import com.hearthsim.model.BoardModel
+import com.hearthsim.Game
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+class IllidanStormrageSpec extends CardSpec {
+
+	HearthTreeNode root
+	BoardModel startingBoard
+
+	def setup() {
+
+		def minionMana = 2;
+		def attack = 5;
+		def health0 = 3;
+		def health1 = 7;
+
+		def commonField = [
+				[mana: minionMana, attack: attack, maxHealth: health0], //todo: attack may be irrelevant here
+		]
+
+		startingBoard = new BoardModelBuilder().make {
+			currentPlayer {
+				hand([IllidanStormrage, TheCoin])
+				field(commonField)
+				mana(7)
+			}
+			waitingPlayer {
+				field(commonField)
+				mana(4)
+			}
+		}
+
+		root = new HearthTreeNode(startingBoard)
+	}
+	
+	def "playing Illidan Stormrage"() {
+		def copiedBoard = startingBoard.deepCopy()
+		def theCard = root.data_.getCurrentPlayerCardHand(0)
+		def ret = theCard.useOn(CURRENT_PLAYER, 1, root, null, null)
+		def ret2 = ret.data_.getCurrentPlayerCardHand(0).useOn(CURRENT_PLAYER, 0, ret, null, null)
+		
+		expect:
+		assertFalse(ret == null);
+
+		assertBoardDelta(copiedBoard, ret.data_) {
+			currentPlayer {
+				playMinion(IllidanStormrage)
+				addMinionToField(FlameOfAzzinoth)
+				removeCardFromHand(TheCoin)
+				mana(2)
+			}
+		}
+	}
+	
+}
