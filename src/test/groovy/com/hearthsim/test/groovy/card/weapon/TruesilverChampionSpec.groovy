9ccb931cd7abb7e61a5dc7f0e457a9a2bdd4cd6f
Adam Babcock <github@mrhen.com>
2015-01-01 11:41:45 -0600

    Add missing Truesilver Champion test

--- src/test/groovy/com/hearthsim/test/groovy/card/weapon/TruesilverChampionSpec.groovy
+++ src/test/groovy/com/hearthsim/test/groovy/card/weapon/TruesilverChampionSpec.groovy
@@ -1,12 +1,14 @@
 package com.hearthsim.test.groovy.card.weapon
 
 import com.hearthsim.card.minion.Minion
+import com.hearthsim.card.minion.concrete.BoulderfistOgre
 import com.hearthsim.card.weapon.concrete.TruesilverChampion
 import com.hearthsim.model.BoardModel
 import com.hearthsim.model.PlayerSide
 import com.hearthsim.test.groovy.card.CardSpec
 import com.hearthsim.test.helpers.BoardModelBuilder
 import com.hearthsim.util.tree.HearthTreeNode
+import spock.lang.Ignore
 
 import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
 
@@ -22,6 +24,10 @@ class TruesilverChampionSpec extends CardSpec {
                 hand([TruesilverChampion])
                 heroHealth(28)
                 mana(4)
+            }
+
+            waitingPlayer {
+                field([[minion: BoulderfistOgre]])
             }
         }
 
@@ -55,9 +61,37 @@ class TruesilverChampionSpec extends CardSpec {
                 heroHealth(26)
             }
         }
-
-
     }
 
+    @Ignore("Existing bug")
+    def 'cannot overheal before attack'() {
+        startingBoard.getCurrentPlayerCharacter(0).setHealth((byte)30);
+        def copiedBoard = startingBoard.deepCopy()
+        def copiedRoot = new HearthTreeNode(copiedBoard)
 
+        def theCard = copiedBoard.getCurrentPlayerCardHand(0);
+        def ret = theCard.useOn(CURRENT_PLAYER, 0, copiedRoot, null, null);
+
+        Minion hero = ret.data_.getCurrentPlayerHero();
+        def target = copiedBoard.getCharacter(PlayerSide.WAITING_PLAYER, 1);
+        ret = hero.attack(PlayerSide.WAITING_PLAYER, target, ret, null, null);
+
+        expect:
+        ret != null
+        assertBoardDelta(startingBoard, copiedBoard) {
+            currentPlayer {
+                heroHealth(24)
+                heroAttack(4)
+                heroHasAttacked(true)
+                weapon(TruesilverChampion) {
+                    weaponCharge(1)
+                }
+                mana(0)
+                removeCardFromHand(TruesilverChampion)
+            }
+            waitingPlayer {
+                updateMinion(0, [deltaHealth: -4])
+            }
+        }
+    }
 }
