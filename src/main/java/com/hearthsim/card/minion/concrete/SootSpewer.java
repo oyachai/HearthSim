3b6175ce2ac07c8eef5749b511d9ede6ab703366
Adam Babcock <github@mrhen.com>
2015-03-01 18:19:23 -0600

    Add simple GvG stealth and spellpower minions

---
+++ src/main/java/com/hearthsim/card/minion/concrete/SootSpewer.java
@@ -0,0 +1,10 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+public class SootSpewer extends Minion {
+    public SootSpewer() {
+        super();
+        spellDamage_ = 1;
+    }
+}
