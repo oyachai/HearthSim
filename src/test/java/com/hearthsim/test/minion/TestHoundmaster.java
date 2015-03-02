b1839b753dab8fe950dcec2a1ec88b5acab45238
Adam Babcock <github@mrhen.com>
2015-03-01 19:19:24 -0600

    Add actual beasts to the Houndmaster test to verify tribe is imported correctly

--- src/test/java/com/hearthsim/test/minion/TestHoundmaster.java
+++ src/test/java/com/hearthsim/test/minion/TestHoundmaster.java
@@ -12,8 +12,9 @@ import org.junit.Test;
 import com.hearthsim.card.Card;
 import com.hearthsim.card.Deck;
 import com.hearthsim.card.minion.Minion;
-import com.hearthsim.card.minion.Minion.MinionTribe;
+import com.hearthsim.card.minion.concrete.BloodfenRaptor;
 import com.hearthsim.card.minion.concrete.Houndmaster;
+import com.hearthsim.card.minion.concrete.StonetuskBoar;
 import com.hearthsim.exception.HSException;
 import com.hearthsim.model.BoardModel;
 import com.hearthsim.model.PlayerSide;
@@ -26,17 +27,14 @@ public class TestHoundmaster {
     private static final byte mana = 2;
     private static final byte attack0 = 2;
     private static final byte health0 = 5;
-    private static final byte health1 = 1;
 
     @Before
     public void setup() throws HSException {
         board = new HearthTreeNode(new BoardModel());
         Minion minion0 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
-        Minion minion1 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
-        minion1.setTribe(MinionTribe.BEAST);
+        Minion minion1 = new StonetuskBoar();
         Minion minion2 = new Minion("" + 0, mana, attack0, health0, attack0, health0, health0);
-        Minion minion3 = new Minion("" + 0, mana, attack0, health1, attack0, health1, health1);
-        minion3.setTribe(MinionTribe.BEAST);
+        Minion minion3 = new BloodfenRaptor();
         Minion minion4 = new Minion("" + 0, mana, attack0, (byte)(health0-2), attack0, health0, health0);
 
         Houndmaster fb = new Houndmaster();
@@ -117,15 +115,15 @@ public class TestHoundmaster {
         assertEquals(res.data_.getCurrentPlayer().getMinions().get(1).getHealth(), health0);
         assertEquals(res.data_.getCurrentPlayer().getMinions().get(1).getTotalAttack(), attack0);
         assertFalse(res.data_.getCurrentPlayer().getMinions().get(1).getTaunt());
-        assertEquals(res.data_.getCurrentPlayer().getMinions().get(2).getHealth(), health0);
-        assertEquals(res.data_.getCurrentPlayer().getMinions().get(2).getTotalAttack(), attack0);
+        assertEquals(res.data_.getCurrentPlayer().getMinions().get(2).getHealth(), 1);
+        assertEquals(res.data_.getCurrentPlayer().getMinions().get(2).getTotalAttack(), 1);
         assertFalse(res.data_.getCurrentPlayer().getMinions().get(2).getTaunt());
 
         assertEquals(res.data_.getWaitingPlayer().getMinions().get(0).getHealth(), health0);
         assertEquals(res.data_.getWaitingPlayer().getMinions().get(0).getTotalAttack(), attack0);
         assertFalse(res.data_.getWaitingPlayer().getMinions().get(0).getTaunt());
-        assertEquals(res.data_.getWaitingPlayer().getMinions().get(1).getHealth(), health1);
-        assertEquals(res.data_.getWaitingPlayer().getMinions().get(1).getTotalAttack(), attack0);
+        assertEquals(res.data_.getWaitingPlayer().getMinions().get(1).getHealth(), 2);
+        assertEquals(res.data_.getWaitingPlayer().getMinions().get(1).getTotalAttack(), 3);
         assertFalse(res.data_.getWaitingPlayer().getMinions().get(1).getTaunt());
         assertEquals(res.data_.getWaitingPlayer().getMinions().get(2).getHealth(), health0-2);
         assertEquals(res.data_.getWaitingPlayer().getMinions().get(2).getTotalAttack(), attack0);
@@ -141,15 +139,15 @@ public class TestHoundmaster {
         assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(1).getHealth(), health0);
         assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(1).getTotalAttack(), attack0);
         assertFalse(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(1).getTaunt());
-        assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(2).getHealth(), health0 + 2);
-        assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(2).getTotalAttack(), attack0 + 2);
+        assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(2).getHealth(), 1 + 2);
+        assertEquals(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(2).getTotalAttack(), 1 + 2);
         assertTrue(res.getChildren().get(0).data_.getCurrentPlayer().getMinions().get(2).getTaunt());
 
         assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(0).getHealth(), health0);
         assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(0).getTotalAttack(), attack0);
         assertFalse(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(0).getTaunt());
-        assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(1).getHealth(), health1);
-        assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(1).getTotalAttack(), attack0);
+        assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(1).getHealth(), 2);
+        assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(1).getTotalAttack(), 3);
         assertFalse(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(1).getTaunt());
         assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(2).getHealth(), health0-2);
         assertEquals(res.getChildren().get(0).data_.getWaitingPlayer().getMinions().get(2).getTotalAttack(), attack0);
