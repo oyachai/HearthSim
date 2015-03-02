c295d47c6d55bb26a62d22634af4a65416df2c2c
Adam Babcock <github@mrhen.com>
2015-03-01 18:37:52 -0600

    Add simple GvG windfury cards

---
+++ src/main/java/com/hearthsim/card/minion/concrete/WhirlingZapOMatic.java
@@ -0,0 +1,10 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+public class WhirlingZapOMatic extends Minion {
+    public WhirlingZapOMatic() {
+        super();
+        this.tribe = MinionTribe.MECH;
+    }
+}
