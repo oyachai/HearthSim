62a7f9dd3ee7213a61786036a0ace33e7b33c7f5
Adam Babcock <github@mrhen.com>
2015-03-01 18:04:05 -0600

    Add GvG vanilla minions

---
+++ src/main/java/com/hearthsim/card/minion/concrete/SpiderTank.java
@@ -0,0 +1,11 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+public class SpiderTank extends Minion {
+
+    public SpiderTank() {
+        super();
+        this.tribe = MinionTribe.MECH;
+    }
+}
