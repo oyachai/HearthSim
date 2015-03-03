95cd3d38b75e6458b641e0d08f3700ce2c9ca399
Adam Babcock <github@mrhen.com>
2015-03-03 17:33:25 -0600

    Implement Clockwork Giant

---
+++ src/test/groovy/com/hearthsim/test/groovy/card/ClockworkGiantSpec.groovy
@@ -0,0 +1,66 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.minion.concrete.ClockworkGiant
+import com.hearthsim.card.spellcard.concrete.TheCoin
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static org.junit.Assert.*
+
+
+class ClockworkGiantSpec extends CardSpec {
+
+    def "playing Clockwork Giant with no other cards in hand -- can't play"() {
+
+        def startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([ClockworkGiant])
+                mana(10)
+            }
+            waitingPlayer {
+                mana(10)
+            }
+        }
+
+        def root = new HearthTreeNode(startingBoard)
+
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 0, root, null, null)
+
+        expect:
+        assertTrue(ret == null);
+
+    }
+
+    def "playing Clockwork Giant with 3 cards in hand"() {
+
+        def startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([ClockworkGiant])
+                mana(10)
+            }
+            waitingPlayer {
+                hand([TheCoin, TheCoin])
+                mana(10)
+            }
+        }
+
+        def root = new HearthTreeNode(startingBoard)
+
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 0, root, null, null)
+
+        expect:
+        assertFalse(ret == null);
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                playMinion(ClockworkGiant)
+                mana(0)
+            }
+        }
+    }
+
+}
