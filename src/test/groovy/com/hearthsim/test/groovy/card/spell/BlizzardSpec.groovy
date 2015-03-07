e440a54311df9271a52299b6f3ac47df3a83896c
Adam Babcock <github@mrhen.com>
2015-03-07 11:29:37 -0600

    Implement Blizzard

---
+++ src/test/groovy/com/hearthsim/test/groovy/card/spell/BlizzardSpec.groovy
@@ -0,0 +1,55 @@
+package com.hearthsim.test.groovy.card.spell
+
+import com.hearthsim.test.groovy.card.CardSpec
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+import com.hearthsim.card.minion.concrete.StranglethornTiger
+import com.hearthsim.card.minion.concrete.WarGolem
+import com.hearthsim.card.spellcard.concrete.Blizzard
+import com.hearthsim.model.BoardModel
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+class BlizzardSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([Blizzard])
+                mana(6)
+            }
+            waitingPlayer {
+                field([[minion: WarGolem],[minion: StranglethornTiger]])
+            }
+        }
+
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "freezes unfrozen minion"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(WAITING_PLAYER, 0, root, null, null)
+
+        expect:
+        assertEquals(root, ret);
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                removeCardFromHand(Blizzard)
+                mana(0)
+            }
+            waitingPlayer {
+                updateMinion(0, [frozen : true, deltaHealth:-2])
+                updateMinion(1, [frozen : true, deltaHealth:-2])
+            }
+        }
+    }
+}
