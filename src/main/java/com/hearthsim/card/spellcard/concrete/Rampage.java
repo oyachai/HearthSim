bcfc7a6dd797e6fc87bcaaca6e095d71630a16a4
Adam Babcock <github@mrhen.com>
2015-03-07 12:33:58 -0600

    Implement Rampage

---
+++ src/main/java/com/hearthsim/card/spellcard/concrete/Rampage.java
@@ -0,0 +1,52 @@
+package com.hearthsim.card.spellcard.concrete;
+
+import com.hearthsim.card.Deck;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.card.spellcard.SpellDamage;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.BoardModel;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.CardDrawNode;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class Rampage extends SpellCard {
+
+    public Rampage() {
+        super();
+
+        this.canTargetEnemyHero = false;
+        this.canTargetOwnHero = false;
+    }
+
+    @Override
+    public boolean canBeUsedOn(PlayerSide playerSide, Minion minion, BoardModel boardModel) {
+        if (!super.canBeUsedOn(playerSide, minion, boardModel)) {
+            return false;
+        }
+
+        if (minion.getHealth() == minion.getMaxHealth()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected HearthTreeNode use_core(
+        PlayerSide side,
+        Minion targetMinion,
+        HearthTreeNode boardState,
+        Deck deckPlayer0,
+        Deck deckPlayer1,
+        boolean singleRealizationOnly)
+        throws HSException {
+        HearthTreeNode toRet = super.use_core(side, targetMinion, boardState, deckPlayer0, deckPlayer1, singleRealizationOnly);
+        if (toRet != null) {
+            targetMinion.setAttack((byte)(targetMinion.getAttack() + 3));
+            targetMinion.setHealth((byte)(targetMinion.getHealth() + 3));
+            targetMinion.setMaxHealth((byte)(targetMinion.getMaxHealth() + 3));
+        }
+        return toRet;
+    }
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/spell/RampageSpec.groovy
@@ -0,0 +1,59 @@
+package com.hearthsim.test.groovy.card.spell
+
+import com.hearthsim.test.groovy.card.CardSpec
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+import static org.junit.Assert.*
+
+import com.hearthsim.card.minion.concrete.StranglethornTiger
+import com.hearthsim.card.minion.concrete.WarGolem
+import com.hearthsim.card.spellcard.concrete.Rampage
+import com.hearthsim.model.BoardModel
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+class RampageSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([Rampage])
+                field([[minion: WarGolem], [minion: WarGolem, health: 3]])
+                mana(3)
+            }
+        }
+
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "can target damaged minion"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 2, root, null, null)
+
+        expect:
+        assertEquals(root, ret);
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                removeCardFromHand(Rampage)
+                mana(1)
+                updateMinion(1, [deltaHealth: +3, deltaAttack: +3, deltaMaxHealth: +3])
+            }
+        }
+    }
+
+    def "cannot target undamaged minion"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 1, root, null, null)
+
+        expect:
+        assertNull(ret);
+    }
+}
