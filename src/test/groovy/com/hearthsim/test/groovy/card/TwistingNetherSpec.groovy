ef8050fadbfc96401df762d24817b4c6defd0257
Adam Babcock <github@mrhen.com>
2015-03-06 23:41:59 -0600

    Implement Twisting Nether

---
+++ src/test/groovy/com/hearthsim/test/groovy/card/TwistingNetherSpec.groovy
@@ -0,0 +1,56 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.model.BoardModel;
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode;
+import com.hearthsim.card.minion.concrete.GoldshireFootman
+import com.hearthsim.card.minion.concrete.WarGolem
+import com.hearthsim.card.spellcard.concrete.TwistingNether
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static org.junit.Assert.*
+
+class TwistingNetherSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([TwistingNether])
+                field([[minion: GoldshireFootman], [minion: WarGolem]])
+                mana(10)
+            }
+            waitingPlayer {
+                field([[minion: GoldshireFootman], [minion: WarGolem]])
+            }
+        }
+
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "playing Twisting Nether kills all minions"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayerCardHand(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 0, root, null, null)
+
+        expect:
+        assertNotNull(ret);
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                removeCardFromHand(TwistingNether)
+                removeMinion(1)
+                removeMinion(0)
+                mana(2)
+            }
+
+            waitingPlayer {
+                removeMinion(1)
+                removeMinion(0)
+            }
+        }
+    }
+}
