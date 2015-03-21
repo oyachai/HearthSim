8c00e5367dcf7b7ba70f3cec26a9fff3b9447008
oyachai <oyachai@gmail.com>
2015-03-21 15:03:59 +0000

    Implemented Defias Ringleader

---
+++ src/main/java/com/hearthsim/card/minion/concrete/DefiasBandit.java
@@ -0,0 +1,14 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+
+/**
+ * Created by oyachai on 3/21/15.
+ */
+public class DefiasBandit extends Minion {
+
+    public DefiasBandit() {
+        super();
+    }
+
+}
---
+++ src/main/java/com/hearthsim/card/minion/concrete/DefiasRingleader.java
@@ -0,0 +1,39 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionUntargetableBattlecry;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.PlayerModel;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+/**
+ * Created by oyachai on 3/21/15.
+ */
+public class DefiasRingleader extends Minion implements MinionUntargetableBattlecry {
+
+    public DefiasRingleader() {
+        super();
+    }
+
+    @Override
+    public boolean hasBattlecry() {
+        return false;
+    }
+
+    @Override
+    public HearthTreeNode useUntargetableBattlecry_core(
+        int minionPlacementIndex,
+        HearthTreeNode boardState,
+        boolean singleRealizationOnly
+    ) throws HSException {
+        HearthTreeNode toRet = boardState;
+
+        if (toRet.data_.getCurrentPlayer().isComboEnabled()) {
+            Minion newMinion = new DefiasBandit();
+            toRet = newMinion.summonMinion(PlayerSide.CURRENT_PLAYER, this, toRet, false, true);
+        }
+        return toRet;
+    }
+
+}
