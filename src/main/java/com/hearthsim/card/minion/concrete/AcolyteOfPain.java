c232da998603e4668d243dc9b38800faf1641a51
Adam Babcock <github@mrhen.com>
2015-03-29 16:01:55 -0500

    Convert Acolyte of Pain and Northshire Cleric to effect pattern

--- src/main/java/com/hearthsim/card/minion/concrete/AcolyteOfPain.java
+++ src/main/java/com/hearthsim/card/minion/concrete/AcolyteOfPain.java
@@ -1,12 +1,13 @@
 package com.hearthsim.card.minion.concrete;
 
 import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionDamagedInterface;
 import com.hearthsim.event.effect.CardEffectCharacter;
 import com.hearthsim.event.effect.CardEffectCharacterDraw;
 import com.hearthsim.model.PlayerSide;
 import com.hearthsim.util.tree.HearthTreeNode;
 
-public class AcolyteOfPain extends Minion {
+public class AcolyteOfPain extends Minion implements MinionDamagedInterface {
 
     private static final CardEffectCharacter effect = new CardEffectCharacterDraw(1);
 
@@ -15,23 +16,13 @@ public class AcolyteOfPain extends Minion {
     }
 
     /**
-     * Called when this minion takes damage
-     *
      * Draw a card whenever this minion takes damage
-     * @param damage The amount of damage to take
-     * @param attackPlayerSide The player index of the attacker.  This is needed to do things like +spell damage.
-     * @param thisPlayerSide
-     * @param boardState
-     * @param isSpellDamage True if this is a spell damage   @throws HSException    */
+     * */
     @Override
-    public HearthTreeNode takeDamageAndNotify(byte damage, PlayerSide attackPlayerSide, PlayerSide thisPlayerSide, HearthTreeNode boardState, boolean isSpellDamage, boolean handleMinionDeath) {
-        boolean hadShield = this.getDivineShield();
-        HearthTreeNode toRet = super.takeDamageAndNotify(damage, attackPlayerSide, thisPlayerSide, boardState, isSpellDamage, handleMinionDeath);
-
-        if (!hadShield && damage > 0) {
-            toRet = AcolyteOfPain.effect.applyEffect(thisPlayerSide, this, thisPlayerSide, 0, toRet);
+    public HearthTreeNode minionDamagedEvent(PlayerSide thisMinionPlayerSide, PlayerSide damagedPlayerSide, Minion damagedMinion, HearthTreeNode boardState) {
+        if (damagedMinion == this) {
+            boardState = AcolyteOfPain.effect.applyEffect(thisMinionPlayerSide, this, thisMinionPlayerSide, 0, boardState);
         }
-
-        return toRet;
+        return boardState;
     }
 }
