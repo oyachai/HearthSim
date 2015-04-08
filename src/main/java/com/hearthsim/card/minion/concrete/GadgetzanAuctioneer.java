6ac4c8ef429ed15d4c1a482970ac4dd515a8299f
Adam Babcock <github@mrhen.com>
2015-04-07 20:29:50 -0500

    Implement Gadgetzan Auctioneer

---
+++ src/main/java/com/hearthsim/card/minion/concrete/GadgetzanAuctioneer.java
@@ -0,0 +1,29 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.CardPlayBeginInterface;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.spellcard.SpellCard;
+import com.hearthsim.event.effect.CardEffectCharacter;
+import com.hearthsim.event.effect.CardEffectCharacterBuffDelta;
+import com.hearthsim.event.effect.CardEffectCharacterDraw;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class GadgetzanAuctioneer extends Minion implements CardPlayBeginInterface {
+
+    private final static CardEffectCharacter effect = new CardEffectCharacterDraw(1);
+
+    public GadgetzanAuctioneer() {
+        super();
+    }
+
+    @Override
+    public HearthTreeNode onCardPlayBegin(PlayerSide thisCardPlayerSide, PlayerSide cardUserPlayerSide, Card usedCard, HearthTreeNode boardState, boolean singleRealizationOnly) {
+        if (!this.isInHand() && thisCardPlayerSide == cardUserPlayerSide && usedCard instanceof SpellCard) {
+            return GadgetzanAuctioneer.effect.applyEffect(cardUserPlayerSide, usedCard, thisCardPlayerSide, this, boardState);
+        }
+
+        return boardState;
+    }
+}
