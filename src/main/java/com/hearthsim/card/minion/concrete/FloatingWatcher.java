54c1179e8d8b136216627ecba8ed34c078a02408
Adam Babcock <github@mrhen.com>
2015-04-14 19:29:25 -0500

    Implement Floating Watcher

---
+++ src/main/java/com/hearthsim/card/minion/concrete/FloatingWatcher.java
@@ -0,0 +1,37 @@
+package com.hearthsim.card.minion.concrete;
+
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionDamagedInterface;
+import com.hearthsim.event.effect.EffectCharacter;
+import com.hearthsim.event.effect.EffectCharacterBuffDelta;
+import com.hearthsim.event.effect.EffectHeroDraw;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class FloatingWatcher extends Minion implements MinionDamagedInterface {
+
+    private static final EffectCharacter<Minion> effect = new EffectCharacterBuffDelta<>(2, 2);
+
+    public FloatingWatcher() {
+        super();
+    }
+
+    @Override
+    public HearthTreeNode minionDamagedEvent(PlayerSide thisMinionPlayerSide, PlayerSide damagedPlayerSide, Minion damagedMinion, HearthTreeNode boardState) {
+        if (thisMinionPlayerSide != PlayerSide.CURRENT_PLAYER) {
+            return boardState;
+        }
+
+        if (thisMinionPlayerSide != damagedPlayerSide) {
+            return boardState;
+        }
+
+        if (damagedMinion != boardState.data_.modelForSide(damagedPlayerSide).getHero()) {
+            return boardState;
+        }
+
+        boardState = FloatingWatcher.effect.applyEffect(thisMinionPlayerSide, this, thisMinionPlayerSide, this, boardState);
+
+        return boardState;
+    }
+}
