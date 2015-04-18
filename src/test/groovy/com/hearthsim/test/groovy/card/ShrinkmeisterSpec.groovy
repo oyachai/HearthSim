a35583e8e5fd918ce5f1e690374ad8358da74122
Adam Babcock <github@mrhen.com>
2015-04-17 23:29:48 -0500

    Implement Shrinkmeister

---
+++ src/test/groovy/com/hearthsim/test/groovy/card/ShrinkmeisterSpec.groovy
@@ -0,0 +1,57 @@
+package com.hearthsim.test.groovy.card
+
+import com.hearthsim.card.minion.concrete.Shrinkmeister
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static org.junit.Assert.*
+
+import com.hearthsim.card.minion.concrete.AbusiveSergeant
+import com.hearthsim.card.minion.concrete.BoulderfistOgre
+import com.hearthsim.model.BoardModel;
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode;
+
+class ShrinkmeisterSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                field([[minion: BoulderfistOgre]])
+                mana(10)
+            }
+        }
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "subtracts extra attack"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = new Shrinkmeister()
+        def ret = theCard.battlecryEffect.applyEffect(CURRENT_PLAYER, theCard, CURRENT_PLAYER, 1, root)
+
+        expect:
+        assertEquals(root, ret);
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                updateMinion(0, [deltaExtraAttack: -2])
+            }
+        }
+    }
+
+    def "buff is additive"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def target = root.data_.modelForSide(CURRENT_PLAYER).getCharacter(1)
+        target.extraAttackUntilTurnEnd = 2
+        def theCard = new Shrinkmeister()
+        def ret = theCard.battlecryEffect.applyEffect(CURRENT_PLAYER, theCard, CURRENT_PLAYER, 1, root)
+
+        expect:
+        assertEquals(root, ret);
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+        }
+    }
+}
