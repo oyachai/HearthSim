f3347af0e3bdf6a4bee61ccb933c95a4621db60d
Adam Babcock <github@mrhen.com>
2015-04-18 11:58:06 -0500

    Add effect for summoning new instances

--- src/main/java/com/hearthsim/card/spellcard/concrete/FeralSpirit.java
+++ src/main/java/com/hearthsim/card/spellcard/concrete/FeralSpirit.java
@@ -7,11 +7,14 @@ import com.hearthsim.card.spellcard.SpellCard;
 import com.hearthsim.card.spellcard.SpellTargetableCard;
 import com.hearthsim.event.effect.EffectCharacter;
 import com.hearthsim.event.effect.EffectCharacterSummon;
+import com.hearthsim.event.effect.EffectCharacterSummonNew;
 import com.hearthsim.event.filter.FilterCharacter;
 import com.hearthsim.event.filter.FilterCharacterTargetedSpell;
 import com.hearthsim.model.PlayerModel;
 
 public class FeralSpirit extends SpellTargetableCard {
+
+    private static final EffectCharacter effect = new EffectCharacterSummonNew<>(SpiritWolf.class, 2, true);
 
     public FeralSpirit() {
         super();
@@ -24,20 +27,6 @@ public class FeralSpirit extends SpellTargetableCard {
 
     @Override
     public EffectCharacter getTargetableEffect() {
-        return (originSide, origin, targetSide, targetCharacterIndex, boardState) -> {
-            PlayerModel currentPlayer = boardState.data_.modelForSide(targetSide);
-            if (currentPlayer.isBoardFull()) {
-                return null;
-            }
-
-            Minion mi0 = new SpiritWolf();
-            boardState = mi0.summonMinionAtEnd(targetSide, boardState, false, false);
-
-            if (!currentPlayer.isBoardFull()) {
-                Minion mi1 = new SpiritWolf();
-                boardState = mi1.summonMinionAtEnd(targetSide, boardState, false, false);
-            }
-            return boardState;
-        };
+        return FeralSpirit.effect;
     }
 }
