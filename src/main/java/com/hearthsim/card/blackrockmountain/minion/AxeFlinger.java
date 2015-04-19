3fcdaf4126c102c3f2ccc97d23c366d10dfa15c7
Adam Babcock <github@mrhen.com>
2015-04-19 11:27:17 -0500

    Implement Axe Flinger

---
+++ src/main/java/com/hearthsim/card/blackrockmountain/minion/AxeFlinger.java
@@ -0,0 +1,26 @@
+package com.hearthsim.card.blackrockmountain.minion;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionDamagedInterface;
+import com.hearthsim.event.effect.EffectCharacter;
+import com.hearthsim.event.effect.EffectCharacterDamage;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+public class AxeFlinger extends Minion implements MinionDamagedInterface {
+
+    private static final EffectCharacter<Card> effect = new EffectCharacterDamage<>(2);
+
+    public AxeFlinger() {
+        super();
+    }
+
+    @Override
+    public HearthTreeNode minionDamagedEvent(PlayerSide thisMinionPlayerSide, PlayerSide damagedPlayerSide, Minion damagedMinion, HearthTreeNode boardState) {
+        if (damagedMinion == this) {
+            boardState = AxeFlinger.effect.applyEffect(thisMinionPlayerSide, this, thisMinionPlayerSide.getOtherPlayer(), 0, boardState);
+        }
+        return boardState;
+    }
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/blackrockmountain/minion/AxeFlingerSpec.groovy
@@ -0,0 +1,77 @@
+package com.hearthsim.test.groovy.card.blackrockmountain.minion
+
+import com.hearthsim.card.basic.spell.HolySmite
+import com.hearthsim.card.blackrockmountain.minion.AxeFlinger
+import com.hearthsim.model.BoardModel
+import com.hearthsim.test.groovy.card.CardSpec
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+import static com.hearthsim.model.PlayerSide.WAITING_PLAYER
+
+class AxeFlingerSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([HolySmite])
+                field([[minion:AxeFlinger]])
+                mana(10)
+            }
+            waitingPlayer {
+                field([[minion:AxeFlinger]])
+            }
+        }
+
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "deals damage on damage"() {
+        def minionPlayedBoard = startingBoard.deepCopy()
+        def copiedRoot = new HearthTreeNode(minionPlayedBoard)
+        def theCard = minionPlayedBoard.getCurrentPlayer().getHand().get(0);
+        def ret = theCard.useOn(CURRENT_PLAYER, 1, copiedRoot);
+
+        expect:
+        ret != null
+
+        assertBoardDelta(startingBoard, minionPlayedBoard) {
+            currentPlayer {
+                removeCardFromHand(HolySmite)
+                updateMinion(0, [deltaHealth: -2])
+                mana(9)
+                numCardsUsed(1)
+            }
+            waitingPlayer {
+                heroHealth(28)
+            }
+        }
+    }
+
+    def "deals damage on opponent's turn"() {
+        def minionPlayedBoard = startingBoard.deepCopy()
+        def copiedRoot = new HearthTreeNode(minionPlayedBoard)
+        def theCard = minionPlayedBoard.getCurrentPlayer().getHand().get(0);
+        def ret = theCard.useOn(WAITING_PLAYER, 1, copiedRoot);
+
+        expect:
+        ret != null
+
+        assertBoardDelta(startingBoard, minionPlayedBoard) {
+            currentPlayer {
+                removeCardFromHand(HolySmite)
+                mana(9)
+                numCardsUsed(1)
+                heroHealth(28)
+            }
+            waitingPlayer {
+                updateMinion(0, [deltaHealth: -2])
+            }
+        }
+    }
+}
