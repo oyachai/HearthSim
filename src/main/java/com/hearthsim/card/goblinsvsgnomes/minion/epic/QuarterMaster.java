e32179eeed062dde1440c55fc70e1da8c15f0dad
oyachai <oyachai@gmail.com>
2015-06-07 17:23:45 +0100

    implemented Quartermaster

---
+++ src/main/java/com/hearthsim/card/goblinsvsgnomes/minion/epic/QuarterMaster.java
@@ -0,0 +1,36 @@
+package com.hearthsim.card.goblinsvsgnomes.minion.epic;
+
+import com.hearthsim.card.basic.minion.SilverHandRecruit;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionBattlecryInterface;
+import com.hearthsim.event.effect.EffectCharacter;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+
+/**
+ * Created by oyachai on 6/7/15.
+ */
+public class Quartermaster extends Minion implements MinionBattlecryInterface {
+
+    public Quartermaster() {
+        super();
+    }
+
+    /**
+     * Battlecry: Gain +2/+2 to your Silverhand Recruits.
+     */
+    @Override
+    public EffectCharacter<Minion> getBattlecryEffect() {
+        return (PlayerSide originSide, Minion origin, PlayerSide targetSide, int minionPlacementIndex, HearthTreeNode boardState) -> {
+            for (Minion minion : boardState.data_.getCurrentPlayer().getMinions()) {
+                if (minion instanceof SilverHandRecruit) {
+                    minion.addMaxHealth((byte)2);
+                    minion.addHealth((byte)2);
+                    minion.addAttack((byte)2);
+                }
+            }
+            return boardState;
+        };
+    }
+
+}
---
+++ src/test/groovy/com/hearthsim/test/groovy/card/goblinsvsgnomes/minion/QuartermasterSpec.groovy
@@ -0,0 +1,55 @@
+package com.hearthsim.test.groovy.card.goblinsvsgnomes.minion
+
+import com.hearthsim.card.basic.minion.GoldshireFootman
+import com.hearthsim.card.basic.minion.SilverHandRecruit
+import com.hearthsim.card.goblinsvsgnomes.minion.epic.Quartermaster
+import com.hearthsim.model.BoardModel
+import com.hearthsim.test.groovy.card.CardSpec
+import com.hearthsim.test.helpers.BoardModelBuilder
+import com.hearthsim.util.tree.HearthTreeNode
+
+import static com.hearthsim.model.PlayerSide.CURRENT_PLAYER
+
+/**
+ * Created by oyachai on 6/7/15.
+ */
+class QuartermasterSpec extends CardSpec {
+
+    HearthTreeNode root
+    BoardModel startingBoard
+
+    def setup() {
+        startingBoard = new BoardModelBuilder().make {
+            currentPlayer {
+                hand([Quartermaster])
+                field([[minion:SilverHandRecruit], [minion:SilverHandRecruit], [minion:GoldshireFootman]])
+                mana(10)
+            }
+            waitingPlayer {
+                field([[minion:SilverHandRecruit]])
+            }
+        }
+
+        root = new HearthTreeNode(startingBoard)
+    }
+
+    def "playing Quartermaster buffs your own Silverhand Recruits"() {
+        def copiedBoard = startingBoard.deepCopy()
+        def theCard = root.data_.getCurrentPlayer().getHand().get(0)
+        def ret = theCard.useOn(CURRENT_PLAYER, 0, root)
+
+        expect:
+        ret != null
+
+        assertBoardDelta(copiedBoard, ret.data_) {
+            currentPlayer {
+                playMinion(Quartermaster, 0)
+                mana(5)
+                numCardsUsed(1)
+                updateMinion(1, [deltaHealth: 2, deltaMaxHealth: 2, deltaAttack: 2])
+                updateMinion(2, [deltaHealth: 2, deltaMaxHealth: 2, deltaAttack: 2])
+
+            }
+        }
+    }
+}
