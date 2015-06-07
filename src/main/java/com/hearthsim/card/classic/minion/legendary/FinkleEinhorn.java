a27d662b4cc5fcf0a5e90abdc971e1ff9e492e72
oyachai <oyachai@gmail.com>
2015-06-07 17:02:45 +0100

    implemented The Beast

---
+++ src/main/java/com/hearthsim/card/classic/minion/legendary/FinkleEinhorn.java
@@ -0,0 +1,13 @@
+package com.hearthsim.card.classic.minion.legendary;
+
+import com.hearthsim.card.minion.Minion;
+
+/**
+ * Created by oyachai on 6/7/15.
+ */
+public class FinkleEinhorn extends Minion {
+
+    public FinkleEinhorn() {
+        super();
+    }
+}
