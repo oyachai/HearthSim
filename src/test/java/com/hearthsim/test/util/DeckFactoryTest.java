a371e77f785c0de8ad759ae0fdf072675a464f6d
slaymaker1907 <dyllongagnier@gmail.com>
2015-06-06 20:19:10 -0600

    Started new test and changed filterByHero to allow multiple heroes

--- src/test/java/com/hearthsim/test/util/DeckFactoryTest.java
+++ src/test/java/com/hearthsim/test/util/DeckFactoryTest.java
@@ -3,27 +3,37 @@ package com.hearthsim.test.util;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.hearthsim.card.ImplementedCardList;
 import com.hearthsim.card.ImplementedCardList.*;
+import com.hearthsim.util.DeckFactory;
+import com.hearthsim.util.DeckFactory.DeckFactoryBuilder;
 
 public class DeckFactoryTest 
 {
     private ArrayList<ImplementedCard> referenceCards;
+    private HashSet<String> allHeroes;
     
     @Before
     public void setUp() throws Exception 
     {
         referenceCards = ImplementedCardList.getInstance().getCardList();
+        allHeroes = new HashSet<String>();
+        for(ImplementedCard card : referenceCards)
+        	allHeroes.add(card.charClass_);
     }
 
     @Test
-    public void checkAllClasses() 
+    public void checkOnlySelectedClass() 
     {
-        
+    	for(String hero : allHeroes)
+    	{
+    		
+    	}
     }
 
 }
