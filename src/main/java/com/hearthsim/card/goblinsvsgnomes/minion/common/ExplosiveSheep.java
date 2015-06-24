6f1f228519b27fabbd2835e41806c44cd8d59e90
oyachai <oyachai@gmail.com>
2015-06-24 18:01:54 +0100

    explosive sheep should not hit the heroes

--- src/main/java/com/hearthsim/card/goblinsvsgnomes/minion/common/ExplosiveSheep.java
+++ src/main/java/com/hearthsim/card/goblinsvsgnomes/minion/common/ExplosiveSheep.java
@@ -1,12 +1,12 @@
 package com.hearthsim.card.goblinsvsgnomes.minion.common;
 
 import com.hearthsim.card.minion.Minion;
-import com.hearthsim.event.deathrattle.DeathrattleDamageAll;
+import com.hearthsim.event.deathrattle.DeathrattleDamageAllMinions;
 
 public class ExplosiveSheep extends Minion {
 
     public ExplosiveSheep() {
         super();
-        deathrattleAction_ = new DeathrattleDamageAll((byte) 2);
+        deathrattleAction_ = new DeathrattleDamageAllMinions((byte) 2);
     }
 }
