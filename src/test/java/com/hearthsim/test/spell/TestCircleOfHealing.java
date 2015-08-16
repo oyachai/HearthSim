07dad08395410f62eeb9fdeda96f61336fcbdfc6
Hofls <hofls@ya.ru>
2015-08-16 22:25:42 +0500

    Circle of healing test

---
+++ src/test/java/com/hearthsim/test/spell/TestCircleOfHealing.java
@@ -0,0 +1,82 @@
+package com.hearthsim.test.spell;
+
+import com.hearthsim.card.Card;
+import com.hearthsim.card.CharacterIndex;
+import com.hearthsim.card.classic.spell.common.CircleOfHealing;
+import com.hearthsim.card.minion.Minion;
+import com.hearthsim.card.minion.MinionMock;
+import com.hearthsim.exception.HSException;
+import com.hearthsim.model.BoardModel;
+import com.hearthsim.model.PlayerModel;
+import com.hearthsim.model.PlayerSide;
+import com.hearthsim.util.tree.HearthTreeNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class TestCircleOfHealing {
+
+    private HearthTreeNode board;
+    private PlayerModel currentPlayer;
+    private PlayerModel waitingPlayer;
+
+    private static final byte mana = 2;
+    private static final byte attack0 = 2;
+    private static final byte health0 = 3;
+    private static final byte health1 = 8;
+
+    @Before
+    public void setup() throws HSException {
+        board = new HearthTreeNode(new BoardModel());
+        currentPlayer = board.data_.getCurrentPlayer();
+        waitingPlayer = board.data_.getWaitingPlayer();
+
+        Minion minion0_0 = new MinionMock("" + 0, mana, attack0, health0, attack0, health0, health0);
+        Minion minion0_1 = new MinionMock("" + 0, mana, attack0, (byte)(health1 - 4), attack0, health1, health1);
+        Minion minion1_0 = new MinionMock("" + 0, mana, attack0, health0, attack0, health0, health0);
+        Minion minion1_1 = new MinionMock("" + 0, mana, attack0, (byte)(health1 - 4), attack0, health1, health1);
+
+        board.data_.placeMinion(PlayerSide.CURRENT_PLAYER, minion0_0);
+        board.data_.placeMinion(PlayerSide.CURRENT_PLAYER, minion0_1);
+
+        board.data_.placeMinion(PlayerSide.WAITING_PLAYER, minion1_0);
+        board.data_.placeMinion(PlayerSide.WAITING_PLAYER, minion1_1);
+
+        Card fb = new CircleOfHealing();
+        currentPlayer.placeCardHand(fb);
+
+        currentPlayer.setMana((byte) 7);
+        waitingPlayer.setMana((byte) 4);
+
+        currentPlayer.setMaxMana((byte) 7);
+        waitingPlayer.setMaxMana((byte) 4);
+    }
+
+    @Test
+    public void test1() throws HSException {
+        Card theCard = currentPlayer.getHand().get(0);
+        HearthTreeNode ret = theCard.useOn(PlayerSide.CURRENT_PLAYER, CharacterIndex.HERO, board);
+
+        assertFalse(ret == null);
+
+        assertEquals(currentPlayer.getHand().size(), 0);
+        assertEquals(currentPlayer.getNumMinions(), 2);
+        assertEquals(waitingPlayer.getNumMinions(), 2);
+        assertEquals(currentPlayer.getMana(), 7);
+        assertEquals(waitingPlayer.getMana(), 4);
+        assertEquals(currentPlayer.getHero().getHealth(), 30);
+        assertEquals(waitingPlayer.getHero().getHealth(), 30);
+        assertEquals(currentPlayer.getCharacter(CharacterIndex.MINION_1).getHealth(), health0);
+        assertEquals(currentPlayer.getCharacter(CharacterIndex.MINION_2).getHealth(), health1);
+        assertEquals(waitingPlayer.getCharacter(CharacterIndex.MINION_1).getHealth(), health0);
+        assertEquals(waitingPlayer.getCharacter(CharacterIndex.MINION_2).getHealth(), health1);
+
+        assertEquals(currentPlayer.getCharacter(CharacterIndex.MINION_1).getTotalAttack(), attack0);
+        assertEquals(currentPlayer.getCharacter(CharacterIndex.MINION_2).getTotalAttack(), attack0);
+        assertEquals(waitingPlayer.getCharacter(CharacterIndex.MINION_1).getTotalAttack(), attack0);
+        assertEquals(waitingPlayer.getCharacter(CharacterIndex.MINION_2).getTotalAttack(), attack0);
+    }
+
+}
