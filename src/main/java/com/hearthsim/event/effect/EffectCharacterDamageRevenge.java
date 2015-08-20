df9442c71b9d09a9022353145e6a6824bde79344
oyachai <oyachai@PapaPanda.local>
2015-08-20 11:25:53 -0400

    remove unused import

--- src/main/java/com/hearthsim/event/effect/EffectCharacterDamageRevenge.java
+++ src/main/java/com/hearthsim/event/effect/EffectCharacterDamageRevenge.java
@@ -1,6 +1,5 @@
 package com.hearthsim.event.effect;
 
-import com.hearthsim.card.Card;
 import com.hearthsim.card.CharacterIndex;
 import com.hearthsim.card.minion.Minion;
 import com.hearthsim.model.PlayerSide;
