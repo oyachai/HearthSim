6237e06f2d4922505fc8bc61561d08cefdad01bf
Hofls <hofls@ya.ru>
2015-08-20 22:23:55 +0500

    Implemented Imp Gang Boss & Imp

---
+++ src/main/java/com/hearthsim/card/blackrockmountain/minion/common/Imp.java
@@ -0,0 +1,10 @@
+package com.hearthsim.card.blackrockmountain.minion.common;
+
+import com.hearthsim.card.minion.Minion;
+
+public class Imp extends Minion {
+
+    public Imp() {
+        super();
+    }
+}
