7ba0c3d18f1497296d1fe5230fb2a958309fac73
oyachai <oyachai@PapaPanda.local>
2015-08-23 23:24:45 -0400

    Implemented Tournament Attendee

---
+++ src/main/java/com/hearthsim/card/thegrandtournament/minion/common/TournamentAttendee.java
@@ -0,0 +1,14 @@
+package com.hearthsim.card.thegrandtournament.minion.common;
+
+import com.hearthsim.card.minion.Minion;
+
+/**
+ * Created by oyachai on 8/23/15.
+ */
+public class TournamentAttendee extends Minion {
+
+    public TournamentAttendee() {
+        super();
+    }
+
+}
