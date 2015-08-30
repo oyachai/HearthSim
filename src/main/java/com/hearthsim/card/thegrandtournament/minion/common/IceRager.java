e13e2389036be1f417e1949d2abec65aaa7272d2
Hofls <hofls@ya.ru>
2015-08-30 15:34:10 +0500

    Implemented 7 minions with no effects

---
+++ src/main/java/com/hearthsim/card/thegrandtournament/minion/common/IceRager.java
@@ -0,0 +1,11 @@
+package com.hearthsim.card.thegrandtournament.minion.common;
+
+import com.hearthsim.card.minion.Minion;
+
+public class IceRager extends Minion {
+
+    public IceRager() {
+        super();
+    }
+
+}
---
+++ src/main/java/com/hearthsim/card/thegrandtournament/minion/common/PitFighter.java
@@ -0,0 +1,11 @@
+package com.hearthsim.card.thegrandtournament.minion.common;
+
+import com.hearthsim.card.minion.Minion;
+
+public class PitFighter extends Minion {
+
+    public PitFighter() {
+        super();
+    }
+
+}
